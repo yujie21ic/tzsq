9cf1aa3198c8c00565d45d02176130a5d701422b
fc01 <910677221@qq.com>
2019-01-12 22:52:37 +0800

    ____备份过时____

---
+++ src/____备份过时____/playSound.ts
@@ -0,0 +1,24 @@
+// import * as fs from 'fs'
+
+// let audio = new Audio(
+//     URL.createObjectURL(
+//         new Blob([fs.readFileSync('./123.mp3')])
+//     )
+// )
+
+
+// const _1分钟内只执行一次 = (fn: () => void) => {
+//     let 可以执行 = true
+//     return () => {
+//         if (可以执行) {
+//             fn()
+//             可以执行 = false
+//             setTimeout(() => {
+//                 可以执行 = true
+//             }, 60 * 1000)
+//         }
+//     }
+// }
+
+
+// export const playSound = _1分钟内只执行一次(() => audio.play())
