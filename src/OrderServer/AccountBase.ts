a9ff06e0531959420bbed222a278ac80a7b158a7
fc01 <910677221@qq.com>
2019-01-15 14:53:16 +0800

    AccountBase

--- src/OrderServer/AccountBase.ts
+++ src/OrderServer/AccountBase.ts
@@ -4,18 +4,42 @@ export class AccountBase {
 
     jsonSync = new JSONSync(
         {
+            //资金曲线
             wallet: [] as {
                 time: number
                 total: number
             }[],
+            //
             symbol: {
                 XBTUSD: {
+                    //过时
                     状态: '--' as '--' | '开仓中' | '平仓中',
-                    msg: ''
+                    msg: '',
+                    //新
+                    任务: {
+                        止损: false,
+                        止盈: false,
+                    },
+                    委托: {
+                        活动委托: [] as {}[],
+                        止损委托: [] as {}[],
+                        止盈委托: [] as {}[],
+                    }
                 },
                 ETHUSD: {
+                    //过时
                     状态: '--' as '--' | '开仓中' | '平仓中',
-                    msg: ''
+                    msg: '',
+                    //新
+                    任务: {
+                        止损: false,
+                        止盈: false,
+                    },
+                    委托: {
+                        活动委托: [] as {}[],
+                        止损委托: [] as {}[],
+                        止盈委托: [] as {}[],
+                    }
                 },
             }
         }
