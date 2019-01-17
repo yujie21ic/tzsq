2fe4a91743280f76d2ffa847d4c36a330ec60907
fc01 <910677221@qq.com>
2019-01-17 15:24:25 +0800

    __

--- src/lib/F/kvs.ts
+++ src/lib/F/kvs.ts
@@ -1,4 +1,4 @@
 import { keys } from 'ramda'
 
 export const kvs = <T extends any>(obj: { [k: string]: T }) =>
-    keys(obj).map(k => ({ k, v: obj[k] }))
+    keys(obj).map(k => ({ k: k as keyof T, v: obj[k] }))
