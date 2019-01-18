cec61101649299a4072fa4bb6a8ce2d7268b27ff
fc01 <910677221@qq.com>
2019-01-18 20:36:58 +0800

    __

--- src/K线行情.ts
+++ src/K线行情.ts
@@ -1,25 +1,39 @@
 import { chartInit } from './lib/Chart'
 import { KLineLayer } from './lib/Chart/Layer/KLineLayer'
-import { get笔Index, 合并后的K线, get线段 } from './lib/缠中说禅'
-import { 线段Layer } from './lib/Chart/Layer/线段Layer'
-import { 合并后的Layer } from './lib/Chart/Layer/合并后的Layer'
-import { LineLayer } from './lib/Chart/Layer/LineLayer'
-import { 笔Layer } from './lib/Chart/Layer/笔Layer'
 import { BarLayer } from './lib/Chart/Layer/BarLayer'
 import { BaseType } from './lib/BaseType'
-import { MACD } from './lib/指标/MACD'
-import { TextLayer } from './lib/Chart/Layer/TextLayer'
 import { formatDate } from './lib/F/formatDate'
 import { DBClient } from './DBServer/DBClient'
 import { dialog } from './lib/UI/dialog'
 import { timeID } from './lib/F/timeID'
 import { theme } from './lib/Chart/theme'
 
+// import { MACD } from './lib/指标/MACD' 
+
+// import { get笔Index, 合并后的K线, get线段 } from './lib/缠中说禅'
+// import { 线段Layer } from './lib/Chart/Layer/线段Layer'
+// import { 合并后的Layer } from './lib/Chart/Layer/合并后的Layer'
+// import { LineLayer } from './lib/Chart/Layer/LineLayer'
+// import { 笔Layer } from './lib/Chart/Layer/笔Layer'
+
 let nowSymbol: BaseType.BitmexSymbol = 'ETHUSD'
+
+let S = {
+    left: 200,
+    right: 300,
+    data: [] as BaseType.KLine[],
+}
+
+let isDown = false
+let startX = 0
+let startLeft = 0
+let startRight = 0
+
+
+
 
 const load = async () => {
     S = {
-        ...S,
         left: 0,
         right: 100,
         data: []
@@ -28,7 +42,7 @@ const load = async () => {
     const arr = (await DBClient.func.getKLine({
         type: '1m',
         symbol: nowSymbol,
-        startTime: 0,//Date.now() - 24 * 60 * 60 * 1000 * 4,
+        startTime: Date.now() - 24 * 60 * 60 * 1000 * 4,
         endTime: Date.now(),
     })).data
 
@@ -40,7 +54,6 @@ const load = async () => {
     }
 
     S = {
-        ...S,
         left: Math.max(0, arr.length - 100),
         right: arr.length,
         data: arr
@@ -66,17 +79,11 @@ window.addEventListener('mousedown', e => {
 })
 
 
-let S = {
-    data: [] as BaseType.KLine[],
-    left: 200,
-    right: 300,
-}
-
 chartInit(({ layer }) => {
     const arr = S.data
     const klineData = arr
 
-    const { OSC, DIF, DEM } = MACD(arr.map(v => v.close))
+    // const { OSC, DIF, DEM } = MACD(arr.map(v => v.close))
     const 成交买 = arr.map(v => -v.buySize)
     const 成交卖 = arr.map(v => v.sellSize)
 
@@ -99,54 +106,41 @@ chartInit(({ layer }) => {
                 heightPercentage: 0.6,
                 layerList: [
                     layer(KLineLayer, { data: klineData }),
-                    layer(笔Layer, { data: get笔Index(klineData), color: 0xffff00 }),
-                    layer(线段Layer, { data: get线段(get笔Index(klineData)), color: 0xaa0000 }),
-                    layer(合并后的Layer, { data: 合并后的K线(klineData), color: 0xffff00 }),
-                    layer(TextLayer, { text: '', color: 0xffffff }),
+                    // layer(笔Layer, { data: get笔Index(klineData), color: 0xffff00 }),
+                    // layer(线段Layer, { data: get线段(get笔Index(klineData)), color: 0xaa0000 }),
+                    // layer(合并后的Layer, { data: 合并后的K线(klineData), color: 0xffff00 }),                    
                 ]
             },
             {
-                heightPercentage: 0.2,
+                heightPercentage: 0.4,
                 layerList: [
                     layer(BarLayer, { data: 成交买, color: 0x48aa65 }),
                     layer(BarLayer, { data: 成交卖, color: 0xe56546 }),
                 ]
             },
-            {
-                heightPercentage: 0.2,
-                layerList: [
-                    layer(BarLayer, { data: OSC, color: 0xaaaaaa }),
-                    layer(LineLayer, { data: DIF, color: 0xaa0000 }),
-                    layer(LineLayer, { data: DEM, color: 0xffff00 }),
-                    layer(TextLayer, {
-                        text: '',
-                        color: 0xffffff
-                    })
-                ]
-            },
+            // {
+            //     heightPercentage: 0.2,
+            //     layerList: [
+            //         layer(BarLayer, { data: OSC, color: 0xaaaaaa }),
+            //         layer(LineLayer, { data: DIF, color: 0xaa0000 }),
+            //         layer(LineLayer, { data: DEM, color: 0xffff00 }),
+            //         layer(TextLayer, {
+            //             text: '',
+            //             color: 0xffffff
+            //         })
+            //     ]
+            // },
         ]
     }
 })
 
 window.onmousewheel = (e: any) => {
-    S = {
-        ...S,
-        left: Math.floor(S.left + e['wheelDelta'] / 120 * (S.right - S.left) * 0.05)
-    }
+    S.left = Math.floor(S.left + e['wheelDelta'] / 120 * (S.right - S.left) * 0.05)
     S.left = Math.min(S.data.length - 20.5, S.left) //<------------- 
-
     startX = e['clientX']
     startLeft = S.left
     startRight = S.right
-    startWidth = startRight - startLeft
 }
-
-//左右移动
-let isDown = false
-let startLeft = 0
-let startRight = 0
-let startX = 0
-let startWidth = 0
 
 window.onmousedown = e => {
     if (e.button === 0) {
@@ -154,7 +148,6 @@ window.onmousedown = e => {
         startX = e.clientX
         startLeft = S.left
         startRight = S.right
-        startWidth = startRight - startLeft
     }
 }
 
@@ -165,23 +158,11 @@ window.onmouseup = e => {
 }
 
 window.onmousemove = e => {
-
     if (isDown) {
-        let left = startLeft - startWidth * (e.clientX - startX) / (document.body.clientWidth - theme.RIGHT_WIDTH)
-        let right = startRight - startWidth * (e.clientX - startX) / (document.body.clientWidth - theme.RIGHT_WIDTH)
-
-        left = Math.min(S.data.length - 20.5, left) //<-------------
-        right = Math.max(20.5, right)
-
-        S = {
-            ...S,
-            left,
-            right
-        }
-    } else {
-        S = {
-            ...S,
-        }
+        const left = startLeft - (startRight - startLeft) * (e.clientX - startX) / (document.body.clientWidth - theme.RIGHT_WIDTH)
+        const right = startRight - (startRight - startLeft) * (e.clientX - startX) / (document.body.clientWidth - theme.RIGHT_WIDTH)
+        S.left = Math.min(S.data.length - 20.5, left) //<-------------
+        S.right = Math.max(20.5, right)
     }
 }
 
