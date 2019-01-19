185264a13cf1a8cf1f81eeb2daa2c29708af77cb
fc01 <910677221@qq.com>
2019-01-19 13:40:18 +0800

    __

--- src/lib/UI/dialog.tsx
+++ src/lib/UI/dialog.tsx
@@ -3,9 +3,7 @@ import * as ReactDOM from 'react-dom'
 import { remote } from 'electron'
 import { DialogContent, DialogContentText, Dialog, DialogTitle, DialogActions, Button, TextField } from '@material-ui/core'
 
-import { MuiPickersUtilsProvider, InlineDateTimePicker } from 'material-ui-pickers'
-import * as MomentUtils from '@date-io/moment'
-import 'moment/locale/zh-cn'
+
 
 export namespace dialog {
 
@@ -26,8 +24,7 @@ export namespace dialog {
     const clear = () => show(<div />)
 
 
-    class XXX extends React.Component<{
-        type: 'text' | 'date'
+    class XXX extends React.Component<{ 
         title: string
         value: string
         onOK: (str: string) => void
@@ -46,22 +43,12 @@ export namespace dialog {
                 <DialogTitle id='form-dialog-title'>{this.props.title}</DialogTitle>
 
                 <DialogContent>
-                    {
-                        this.props.type === 'text' ? <TextField
-                            autoFocus
-                            value={this.state.value}
-                            onChange={e => this.setState({ value: e.target.value })}
-                            fullWidth
-                        /> :
-                            <MuiPickersUtilsProvider utils={MomentUtils} locale='zh-cn'>
-                                <InlineDateTimePicker
-                                    value={this.state.value}
-                                    onChange={(date: Date) => this.setState({ value: date.toISOString() })}
-                                />
-                            </MuiPickersUtilsProvider>
-                    }
-
-
+                    <TextField
+                        autoFocus
+                        value={this.state.value}
+                        onChange={e => this.setState({ value: e.target.value })}
+                        fullWidth
+                    />
                 </DialogContent>
                 <DialogActions>
                     <Button onClick={clear} color='primary'>取消</Button>
@@ -75,8 +62,7 @@ export namespace dialog {
     }
 
 
-    export const showInput = (p: {
-        type: 'text' | 'date'
+    export const showInput = (p: { 
         title: string
         value: string
         onOK: (str: string) => void
