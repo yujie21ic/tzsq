edead1b12fc8ed14bfebd8bb0e64cd019612fe97
fc01 <910677221@qq.com>
2019-01-19 16:47:41 +0800

    __

--- src/main.ts
+++ src/main.ts
@@ -4,30 +4,24 @@ import { kvs } from './lib/F/kvs'
 import { config } from './config'
 import { showWindow } from './windowExt'
 
-const item = (name: string, accountName = '') => ({
-  label: name,
-  click: () => showWindow(name, accountName)
-})
 
 const menu = Menu.buildFromTemplate([
-  item('盘口'),
-  item('K线行情'),
-  item('Tick行情'),
-  item('Tick复盘'),
-  item('提醒'),
+
+  { label: '盘口', click: () => showWindow('盘口', {}) },
+  { label: 'K线行情', click: () => showWindow('K线行情', {}) },
+  { label: 'Tick行情', click: () => showWindow('Tick行情', {}) },
+  { label: 'Tick复盘', click: () => showWindow('Tick复盘', {}) },
+  { label: '提醒', click: () => showWindow('提醒', {}) },
   { type: 'separator' },
   ...kvs(config.account || {}).map(v => ({
     label: v.k,
     submenu: [
-      item('计分板', v.k),
-      item('交易', v.k),
+      { label: '计分板', click: () => showWindow('计分板', { accountName: v.k }) },
+      { label: '交易', click: () => showWindow('交易', { accountName: v.k }) },
     ],
   })),
   { type: 'separator' },
-  {
-    label: '退出',
-    click: () => app.exit()
-  }
+  { label: '退出', click: () => app.exit() }
 ])
 
 
