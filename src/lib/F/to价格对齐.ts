88ebb86d32348a33e914b9de19270ac14d0773d0
fc01 <910677221@qq.com>
2019-01-19 18:28:13 +0800

    to价格对齐 fix浮点

--- src/lib/F/to价格对齐.ts
+++ src/lib/F/to价格对齐.ts
@@ -1,15 +1,17 @@
 import { BaseType } from '../BaseType'
 
+const fix浮点 = (n: number) => Number(n.toFixed(5))
+
 export const to价格对齐 = ({ grid, side, value }: { grid: number, side: BaseType.Side, value: number }) => {
     const ret = Math.floor(value / grid) * grid
     if (side === 'Buy') {       //ret <= value
-        return ret
+        return fix浮点(ret)
     }
     else {                      //ret>= value
         if (ret === value) {
-            return value
+            return fix浮点(value)
         } else {
-            return ret + grid
+            return fix浮点(ret + grid)
         }
     }
 }
