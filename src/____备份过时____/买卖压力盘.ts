1fd91bf69255880fe28b38d31d9e88bad377ac09
fc01 <910677221@qq.com>
2019-01-21 15:14:12 +0800

    _

--- src/____备份过时____/买卖压力盘.ts
+++ src/____备份过时____/买卖压力盘.ts
@@ -1,8 +1,8 @@
-// export const 买卖压力盘 = (p: {
-//     bidSize: number
-//     bidPrice: number
-//     askSize: number
-//     askPrice: number
-// }) =>
-//     ((p.bidSize * p.bidPrice) - (p.askSize * p.askPrice)) /
-//     ((p.bidSize * p.bidPrice) + (p.askSize * p.askPrice))
+export const 买卖压力盘 = (p: {
+    bidSize: number
+    bidPrice: number
+    askSize: number
+    askPrice: number
+}) =>
+    ((p.bidSize * p.bidPrice) - (p.askSize * p.askPrice)) /
+    ((p.bidSize * p.bidPrice) + (p.askSize * p.askPrice))
--- src/____备份过时____/电话提醒.ts
+++ src/____备份过时____/电话提醒.ts
@@ -1,35 +1,37 @@
-// import * as request from 'request'
-// import { compose } from 'ramda'
-// import { F } from '../lib/F'
+import * as request from 'request'
+import { compose } from 'ramda'
+import { typeObjectParse } from '../lib/F/typeObjectParse'
+import { safeJSONParse } from '../lib/F/safeJSONParse'
+import { queryStringStringify } from '../lib/F/queryStringStringify'
 
-// const parse1 = compose(F.typeObjectParse({ count: 0 }), F.safeJSONParse)
+const parse1 = compose(typeObjectParse({ count: 0 }), safeJSONParse)
 
-// const 电话提醒XX = () => new Promise<boolean>(resolve =>
-//     request({
-//         url: 'https://voice.yunpian.com/v2/voice/send.json',
-//         method: 'POST',
-//         headers: {
-//             'Accept': 'application/json;charset=utf-8',
-//             'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8'
-//         },
-//         body: F.queryStringStringify({
-//             apikey: '53797beb6d1ccbd8bf5dee7bc3ebf8f2',
-//             mobile: '18610441417',
-//             code: '1234'
-//         })
-//     }, (error, response, body) =>
-//             resolve(parse1(body).count >= 1)
-//     )
-// )
+const 电话提醒XX = () => new Promise<boolean>(resolve =>
+    request({
+        url: 'https://voice.yunpian.com/v2/voice/send.json',
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json;charset=utf-8',
+            'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8'
+        },
+        body: queryStringStringify({
+            apikey: '53797beb6d1ccbd8bf5dee7bc3ebf8f2',
+            mobile: '18610441417',
+            code: '1234'
+        })
+    }, (error, response, body) =>
+            resolve(parse1(body).count >= 1)
+    )
+)
 
 
-// export const 电话提醒 = async () => {
-//     for (let i = 0; i < 3; i++) {
-//         console.log('电话提醒' + i)
-//         if (await 电话提醒XX()) {
-//             console.log('电话提醒成功')
-//             return
-//         }
-//     }
-//     console.log('电话提醒失败')
-// }
+export const 电话提醒 = async () => {
+    for (let i = 0; i < 3; i++) {
+        console.log('电话提醒' + i)
+        if (await 电话提醒XX()) {
+            console.log('电话提醒成功')
+            return
+        }
+    }
+    console.log('电话提醒失败')
+}
