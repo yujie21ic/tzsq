1ec8e36d46a5d8c4882fa05bb9383f6be2e9a7e0
fc01 <910677221@qq.com>
2019-01-22 12:44:32 +0800

    ____

--- src/OrderServer/task/委托检测step.ts
+++ src/OrderServer/task/委托检测step.ts
@@ -10,25 +10,27 @@ export const 委托检测step = (symbol: BaseType.BitmexSymbol) => async (self: 
         v.type === '限价' || v.type === '限价只减仓' || v.type === '市价触发'
     )
 
-    const 限价只减仓 = self.jsonSync.rawData.symbol[symbol].活动委托.filter(v => v.type === '限价只减仓')
-
     //没有委托
     if (活动委托.length === 0) {
         return false
     }
     else if (活动委托.length === 1) {
-        if (仓位数量 === 0) {
+        if (
             //没有仓位随便
+            仓位数量 === 0 ||
+
+            //有仓位 有委托 只能是 
+            //部分成交的委托 
+            //依赖ws先返回 委托更新 再返回仓位更新 (测试发现是这样子的)
+            (活动委托[0].type === '限价' && 活动委托[0].cumQty !== 0) ||
+
+            //或者 限价减仓委托
+            活动委托[0].type === '限价只减仓'
+        ) {
             return false
         } else {
-            //有仓位 有委托  
-            //只能是 限价减仓委托
-            if (限价只减仓.length === 0) {
-                await BitMEXOrderAPI.cancel(self.cookie, 活动委托.map(v => v.id))
-                return true
-            } else {
-                return false
-            }
+            await BitMEXOrderAPI.cancel(self.cookie, 活动委托.map(v => v.id))
+            return true
         }
     }
     else {
