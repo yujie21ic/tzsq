116f7e7b42d25bbd6a0339cdaef55f3a7fb5144c
fc01 <910677221@qq.com>
2019-01-23 10:36:19 +0800

    __

--- src/lib/C/speak.ts
+++ src/lib/C/speak.ts
@@ -5,8 +5,10 @@ export const speak = (msg: string, id: string) => {
 
     const n = dic.get(id)
     if (n !== undefined && Date.now() - n < 60 * 1000) {
-        console.log('没有提醒', Math.floor((Date.now() - n) / 1000), { body: msg })
+        console.log('没有提醒', Math.floor((Date.now() - n) / 1000), JSON.stringify({ body: msg }))
         return
+    } else {
+        console.log('提醒', JSON.stringify({ body: msg }))
     }
 
     dic.set(id, Date.now())
