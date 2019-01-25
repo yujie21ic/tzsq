e8601b3736349bf6b0bce440d9559ea118a54164
fc01 <910677221@qq.com>
2019-01-25 20:46:41 +0800

    ____

--- src/RealDataServer/DataClient.ts
+++ src/RealDataServer/DataClient.ts
@@ -8,7 +8,6 @@ import { timeID } from '../lib/F/timeID'
 
 export namespace DataClient {
 
-
     export class RealData__Client extends RealDataBase {
 
         private ws = new WebSocketClient({
@@ -30,42 +29,6 @@ export namespace DataClient {
     const 加载多少秒数据 = (config.tick历史加载多少秒 || 300) + 120 //多2分钟
 
     export class RealData__History extends RealDataBase {
-
-        private get_binance_tick = async (symbol: BaseType.BinanceSymbol, startTime: number) => {
-
-            const { data, error, msg } = (await DBClient.func.getBinanceTick({
-                symbol,
-                startTime: startTime - 30 * 1000,   //30秒前 
-                endTime: startTime + (加载多少秒数据 + 30) * 1000    //(加载多少秒数据 + 30)秒后 
-            }))
-
-            if (data === undefined || data.length === 0) {
-                console.info(`get_binance_tick ${symbol} 错误`, { msg, error, data })
-                this.重新初始化()
-                return
-            }
-
-            for (let i = 0; i < data.length; i++) {
-                this.on着笔({
-                    symbol,
-                    xxxxxxxx: this.jsonSync.data.binance[symbol as 'btcusdt'].data,
-                    timestamp: data[i].timestamp,
-                    side: data[i].size > 0 ? 'Buy' : 'Sell',
-                    price: data[i].price,
-                    size: Math.abs(data[i].size),
-                })
-
-                if (this.jsonSync.rawData.binance[symbol as 'btcusdt'].data.length >= 加载多少秒数据 * (1000 / RealDataBase.单位时间)) {
-                    this.重新初始化()
-                    return
-                }
-            }
-
-
-            this.重新初始化()
-            return
-        }
-
 
         private get_bitmex_orderBook = async (symbol: BaseType.BitmexSymbol, startTime: number) => {
 
@@ -102,7 +65,7 @@ export namespace DataClient {
             return
         }
 
-        async getBitmex500msKLine(symbol: BaseType.BitmexSymbol, startTime: number) {
+        async get500msKLine(symbol: BaseType.BitmexSymbol | BaseType.BinanceSymbol, startTime: number) {
             const { data } = await DBClient.func.getKLine({
                 type: '500ms',
                 symbol,
@@ -112,10 +75,12 @@ export namespace DataClient {
             return data || []
         }
 
+
+
         async load(startTime: number) {
             startTime -= (1000 * 120) //2分钟前
 
-            this.data.startTick = Math.floor(startTime / RealDataBase.单位时间)            
+            this.data.startTick = Math.floor(startTime / RealDataBase.单位时间)
 
             this.data.bitmex.XBTUSD.data = []
             this.data.bitmex.ETHUSD.data = []
@@ -126,16 +91,22 @@ export namespace DataClient {
             this.get_bitmex_orderBook('XBTUSD', startTime)
             this.get_bitmex_orderBook('ETHUSD', startTime)
 
-            this.data.bitmex.XBTUSD.data = await this.getBitmex500msKLine('XBTUSD', startTime)
+            this.data.bitmex.XBTUSD.data = await this.get500msKLine('XBTUSD', startTime)
             console.log('XBTUSD 500msKLine', this.data.bitmex.XBTUSD.data)
             this.重新初始化()
 
-            this.data.bitmex.ETHUSD.data = await this.getBitmex500msKLine('ETHUSD', startTime)
+            this.data.bitmex.ETHUSD.data = await this.get500msKLine('ETHUSD', startTime)
             console.log('ETHUSD 500msKLine', this.data.bitmex.ETHUSD.data)
             this.重新初始化()
 
-            this.get_binance_tick('btcusdt', startTime)
-            this.get_binance_tick('ethusdt', startTime)
+            this.data.binance.btcusdt.data = await this.get500msKLine('btcusdt', startTime)
+            console.log('btcusdt 500msKLine', this.data.binance.btcusdt.data)
+            this.重新初始化()
+
+            this.data.binance.ethusdt.data = await this.get500msKLine('ethusdt', startTime)
+            console.log('ethusdt 500msKLine', this.data.binance.ethusdt.data)
+            this.重新初始化()
+
         }
     }
 
