bac75ca0dc714cf6d7994787243b8c04a06a3b27
fc01 <910677221@qq.com>
2019-01-29 20:23:02 +0800

    __

--- src/交易.tsx
+++ src/交易.tsx
@@ -1,5 +1,4 @@
 import * as React from 'react'
-import * as ReactDOM from 'react-dom'
 import { OrderClient } from './OrderServer/OrderClient'
 import { config } from './config'
 import { BaseType } from './lib/BaseType'
