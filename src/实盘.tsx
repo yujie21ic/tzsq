c051e7a9900ef141b51dd8a415ee1bcde3e3d408
fc01 <910677221@qq.com>
2019-01-29 19:39:11 +0800

    __

--- src/实盘.tsx
+++ src/实盘.tsx
@@ -1,1 +1,5 @@
 //实盘tsx
+const UI = <div style={{ display: 'flex', height: '100%' }}>
+    <div style={{ flex: '1 1 auto' }}>我在左边，自适应布局</div>
+    <div style={{ width: '200px' }}>我在右边，定宽</div>
+</div> 
