46b28dcefb9df78e0473519d5d4cf414d47ee0bd
fc01 <910677221@qq.com>
2019-02-09 23:32:17 +0800

    ____

--- src/OrderServer/task/自动交易step.ts
+++ src/OrderServer/task/自动交易step.ts
@@ -1,12 +1,18 @@
 import { BaseType } from '../../lib/BaseType'
 import { Account } from '../Account'
 
-
 export const 自动交易step = (symbol: BaseType.BitmexSymbol) => async (self: Account) => {
 
     if (self.jsonSync.rawData.symbol[symbol].自动交易 === false) {
         return true
     }
+
+    //信号灯亮 挂单  
+    //开仓成功了  挂止盈 波动率/10+5
+
+    //提前 修改 止盈
+    //触发了反向开仓信号
+
 
     return true
 } 
