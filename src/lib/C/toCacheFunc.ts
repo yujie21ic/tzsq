fae76f6e687d6ca1fa5410373fa9f956d8af60ca
fc01 <910677221@qq.com>
2019-02-09 15:16:09 +0800

    __

--- src/lib/C/toCacheFunc.ts
+++ src/lib/C/toCacheFunc.ts
@@ -1,4 +1,5 @@
 //普通函数   转换成   缓存函数返回值的函数  相同参数只调用一遍
+//函数参数 不能是 对象
 export const toCacheFunc = <F extends (...p: any) => any>(f: F) => {
     const dic = new Map<string, any>()
     return ((...p: any) => {
