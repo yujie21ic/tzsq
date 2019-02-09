bfd3e1b38721381a14347b2fe0c388317fcda010
fc01 <910677221@qq.com>
2019-02-09 23:18:53 +0800

    自动交易step

---
+++ src/OrderServer/task/自动交易.ts
@@ -0,0 +1,10 @@
+import { BaseType } from '../../lib/BaseType'
+import { Account } from '../Account'
+
+
+export const 自动交易step = (symbol: BaseType.BitmexSymbol) => async (self: Account) => {
+
+    const ret = true
+
+    return ret
+} 
