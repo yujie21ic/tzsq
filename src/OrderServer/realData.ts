bc46ae712af904f855abbb27c28183b6f2d5138f
fc01 <910677221@qq.com>
2019-02-12 11:47:58 +0800

    __波动率

--- src/OrderServer/realData.ts
+++ src/OrderServer/realData.ts
@@ -6,13 +6,18 @@ import { to价格对齐 } from '../lib/F/to价格对齐'
 
 //_________________________________________________________________________//
 
-const get信号msg = (up: ArrayLike<{ name: string, value: boolean }[]>, down: ArrayLike<{ name: string, value: boolean }[]>) =>
-    JSON.stringify({
+const get信号msg = (symbol: BaseType.BitmexSymbol) => {
+    const up = realData.dataExt[symbol].期货.信号_上涨
+    const down = realData.dataExt[symbol].期货.信号_下跌
+
+    return JSON.stringify({
         信号_上涨: up.length > 0 ? up[up.length - 1] : '',
         信号_下跌: down.length > 0 ? down[down.length - 1] : '',
+        波动率: lastNumber(realData.dataExt[symbol].期货.波动率),
     }, undefined, 4)
+}
 
-    
+
 export const 信号灯side = (symbol: BaseType.BitmexSymbol) => {
 
     //TODO
@@ -21,10 +26,10 @@ export const 信号灯side = (symbol: BaseType.BitmexSymbol) => {
     const down = realData.dataExt[symbol].期货.信号_下跌
 
     if (up.length > 0 && up[up.length - 1].every(v => v.value)) {
-        return { 信号side: 'Sell' as 'Sell', 信号msg: get信号msg(up, down) }
+        return { 信号side: 'Sell' as 'Sell', 信号msg: get信号msg(symbol) }
     }
     else if (down.length > 0 && down[down.length - 1].every(v => v.value)) {
-        return { 信号side: 'Buy' as 'Buy', 信号msg: get信号msg(up, down) }
+        return { 信号side: 'Buy' as 'Buy', 信号msg: get信号msg(symbol) }
     }
     else {
         return { 信号side: 'none' as 'none', 信号msg: '' }
