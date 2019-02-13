dd0f92f90346b05371e4a2123dd4e83d0f24364b
fc01 <910677221@qq.com>
2019-02-13 19:55:57 +0800

    ____

--- src/RealDataServer/RealDataBase.ts
+++ src/RealDataServer/RealDataBase.ts
@@ -460,29 +460,42 @@ export class RealDataBase {
 
 
         const 涨价差 = 指标.lazyMapCache(() => Math.min(最高价10.length, 价格均线60.length), i => Math.abs(最高价10[i] - 价格均线60[i]))
-        const 涨价差__除以__这一段内的成交量 = 指标.lazyMapCache2({ 累计成交量: 0 }, (arr: number[], ext) => {
+
+        const 涨价差__累计成交量 = 指标.lazyMapCache2({ 累计成交量: 0 }, (arr: number[], ext) => {
             for (let i = Math.max(0, arr.length - 1); i < Math.min(涨价差.length, 成交量买.length, 成交量卖.length); i++) {
                 if (涨价差[i] < 0.01) {
                     ext.累计成交量 = 0
                 } else {
                     ext.累计成交量 += Math.abs(成交量买[i] - 成交量卖[i])
                 }
-                arr[i] = ext.累计成交量 === 0 ? NaN : 涨价差[i] / ext.累计成交量
+                arr[i] = ext.累计成交量
             }
         })
 
+        const 涨价差__除以__这一段内的成交量 = 指标.lazyMapCache(
+            () => Math.min(涨价差__累计成交量.length, 涨价差.length),
+            i => 涨价差__累计成交量[i] === 0 ? NaN : 涨价差[i] / 涨价差__累计成交量[i]
+        )
+
+
         //重复
         const 跌价差 = 指标.lazyMapCache(() => Math.min(最低价10.length, 价格均线60.length), i => Math.abs(最低价10[i] - 价格均线60[i]))
-        const 跌价差__除以__这一段内的成交量 = 指标.lazyMapCache2({ 累计成交量: 0 }, (arr: number[], ext) => {
+
+        const 跌价差__累计成交量 = 指标.lazyMapCache2({ 累计成交量: 0 }, (arr: number[], ext) => {
             for (let i = Math.max(0, arr.length - 1); i < Math.min(跌价差.length, 成交量买.length, 成交量卖.length); i++) {
                 if (跌价差[i] < 0.01) {
                     ext.累计成交量 = 0
                 } else {
                     ext.累计成交量 += Math.abs(成交量买[i] - 成交量卖[i])
                 }
-                arr[i] = ext.累计成交量 === 0 ? NaN : 跌价差[i] / ext.累计成交量
+                arr[i] = ext.累计成交量
             }
         })
+
+        const 跌价差__除以__这一段内的成交量 = 指标.lazyMapCache(
+            () => Math.min(跌价差__累计成交量.length, 跌价差.length),
+            i => 跌价差__累计成交量[i] === 0 ? NaN : 跌价差[i] / 跌价差__累计成交量[i]
+        )
 
         const 涨价差__除以__这一段内的成交量12 = 指标.EMA(涨价差__除以__这一段内的成交量, 12, RealDataBase.单位时间)
         const 涨价差__除以__这一段内的成交量26 = 指标.EMA(涨价差__除以__这一段内的成交量, 26, RealDataBase.单位时间)
@@ -757,6 +770,8 @@ export class RealDataBase {
 
 
         return {
+            跌价差__累计成交量,
+            涨价差__累计成交量,
             上涨还是下跌,
             自动下单条件,
             价格, 价格均线, 波动率, 成交量买, 成交量买均线: 成交量买均线30, 成交量卖, 盘口买, 盘口卖, 净盘口, 净盘口均线,
