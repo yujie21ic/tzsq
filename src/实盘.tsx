be7a52f5472b89974441ec47c1cc262c0bb59ac4
fc01 <910677221@qq.com>
2019-02-13 14:12:00 +0800

    __

--- src/实盘.tsx
+++ src/实盘.tsx
@@ -88,11 +88,11 @@ class Item extends React.Component<{ symbol: BaseType.BitmexSymbol, 位置: numb
                         rpc.任务_开关({ cookie, symbol: this.props.symbol, 任务名字: '自动开仓', value: v })
                     }}
                 /> </p>
-                <p>_{任务开关.自动开仓.text}</p>
+                {/* <p>_{任务开关.自动开仓.text}</p> */}
                 <p>自动止盈:<Switch checked={任务开关.自动止盈.value} onChange={(e, v) => {
                     rpc.任务_开关({ cookie, symbol: this.props.symbol, 任务名字: '自动止盈', value: v })
                 }} /></p>
-                <p>_{任务开关.自动止盈.text}</p>
+                {/* <p>_{任务开关.自动止盈.text}</p> */}
             </div>
             <div style={{
                 display: 'flex',
