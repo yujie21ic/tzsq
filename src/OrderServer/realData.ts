5e5eea993728b24507da013117ceb1c64cf9266e
fc01 <910677221@qq.com>
2019-02-21 00:32:09 +0800

    自动开仓改成3根

--- src/OrderServer/realData.ts
+++ src/OrderServer/realData.ts
@@ -1,7 +1,7 @@
 import { RealData } from '../RealDataServer/RealData'
 export const realData = new RealData(false)
 import { BaseType } from '../lib/BaseType'
-import { lastNumber } from '../lib/F/lastNumber' 
+import { lastNumber } from '../lib/F/lastNumber'
 
 //_________________________________________________________________________//
 
@@ -10,8 +10,8 @@ const get信号msg = (symbol: BaseType.BitmexSymbol) => {
     const down = realData.dataExt[symbol].期货.信号_下跌
 
     return JSON.stringify({
-        信号_上涨: up.length > 0 ? up[up.length - 1] : '',
-        信号_下跌: down.length > 0 ? down[down.length - 1] : '',
+        信号_上涨: up.length > 3 ? [up[up.length - 3], up[up.length - 2], up[up.length - 1]] : '',
+        信号_下跌: down.length > 3 ? [down[down.length - 3], down[down.length - 2], down[down.length - 1]] : '',
         波动率: lastNumber(realData.dataExt[symbol].期货.波动率),
     }, undefined, 4)
 }
@@ -21,10 +21,10 @@ export const 信号灯side = (symbol: BaseType.BitmexSymbol) => {
     const up = realData.dataExt[symbol].期货.信号_上涨
     const down = realData.dataExt[symbol].期货.信号_下跌
 
-    if (up.length > 0 && up[up.length - 1].every(v => v.value)) {
+    if (up.length > 3 && up[up.length - 1].every(v => v.value) && up[up.length - 2].every(v => v.value) && up[up.length - 3].every(v => v.value)) {
         return { 信号side: 'Sell' as 'Sell', 信号msg: get信号msg(symbol) }
     }
-    else if (down.length > 0 && down[down.length - 1].every(v => v.value)) {
+    else if (down.length > 3 && down[down.length - 1].every(v => v.value) && down[down.length - 2].every(v => v.value) && down[down.length - 3].every(v => v.value)) {
         return { 信号side: 'Buy' as 'Buy', 信号msg: get信号msg(symbol) }
     }
     else {
