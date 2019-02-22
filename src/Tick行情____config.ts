a433c1d3215b9c411810de988e3d0dfc58097147
schu <workfor2012@gmail.com>
2019-02-22 14:48:08 +0800

    更新图表

--- src/Tick行情____config.ts
+++ src/Tick行情____config.ts
@@ -249,6 +249,82 @@ export const Tick行情____config: { [key in string]: ItemFunc } = {
         
         ]
     }),
+
+
+    实盘hopex: (d, d2) => ({
+        heightList: [0.5, 0.15,0.1,0.15,0.1],
+        items: [
+            [
+                {
+                    numberColor: '0x000000',
+                    layerList: [
+                        layer(LineLayer, { data: d2.XBTUSD.hopex.价格, color: ETH颜色 }),
+                    ]
+                },
+                {
+                numberColor: BTC颜色,
+                layerList: [
+                    layer(LineLayer, { data: d2.XBTUSD.期货.价格, color: BTC颜色 }),
+                    layer(TextLayer, {
+                        text:
+                            `hopex:${lastNumber(d2.XBTUSD.hopex.价格).toFixed(2)}  ` +
+                            `bitmex:${lastNumber(d.期货.价格).toFixed(2)}      ` +
+                            `期货30秒内成交量:${d.期货30秒内成交量().toFixed(2)}万   ` +
+                            `期货波动率:${lastNumber(d.期货.波动率).toFixed(2)}`,
+                        color: d === d2.XBTUSD ? BTC颜色 : ETH颜色,
+                    })
+                ]
+            },
+            {
+                numberColor: 石青,
+                numberX: 100,
+                layerList: [
+                    layer(LineLayer, { data: d.期货.波动率, color: 石青 }),
+                ]
+            }],
+            {
+                layerList: [
+                    layer(信号Layer, { data: d.期货.信号_上涨, color: 卖颜色 }),
+                    layer(TextLayer, {
+                        text:
+                            `上涨摸顶                    `,
+                        color:  ETH颜色,
+                    })
+                ]
+            },
+            {
+                layerList: [
+                    layer(信号Layer, { data: d.期货.信号_下跌, color: 买颜色 }),
+                    layer(TextLayer, {
+                        text:
+                            `下跌抄底                    `,
+                        color:  ETH颜色,
+                    })
+                ]
+            },
+            {
+                layerList: [
+                    layer(信号Layer, { data: d2.XBTUSD.信号hopex_上涨, color: 买颜色 }),
+                    layer(TextLayer, {
+                        text:
+                            `上涨追涨                    `,
+                        color:  ETH颜色,
+                    })
+                ]
+            },
+            {
+                layerList: [
+                    layer(信号Layer, { data: d2.XBTUSD.信号hopex_下跌, color: 卖颜色 }),
+                    layer(TextLayer, {
+                        text:
+                            `下跌追跌                    `,
+                        color:  ETH颜色,
+                    })
+                ]
+            },
+        
+        ]
+    }),
    
     复盘下跌动力: (d, d2) => ({
         heightList: [0.4, 0.2, 0.4],
