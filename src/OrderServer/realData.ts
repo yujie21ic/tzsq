07f2612af9421619204f241bbed730e145a133a4
fc01 <910677221@qq.com>
2019-02-23 02:21:19 +0800

    //抵消

--- src/OrderServer/realData.ts
+++ src/OrderServer/realData.ts
@@ -5,13 +5,17 @@ import { lastNumber } from '../lib/F/lastNumber'
 
 //_________________________________________________________________________//
 
-const get信号msg = (symbol: BaseType.BitmexSymbol) => {
-    const up = realData.dataExt[symbol].期货.信号_上涨
-    const down = realData.dataExt[symbol].期货.信号_下跌
+export const get信号msg = (symbol: BaseType.BitmexSymbol) => {
+    const 上涨 = realData.dataExt[symbol].期货.信号_上涨
+    const 下跌 = realData.dataExt[symbol].期货.信号_下跌
+    const 追涨 = realData.dataExt[symbol].期货.信号_追涨
+    const 追跌 = realData.dataExt[symbol].期货.信号_追跌
 
     return JSON.stringify({
-        信号_上涨: up.length > 3 ? [up[up.length - 3], up[up.length - 2], up[up.length - 1]] : '',
-        信号_下跌: down.length > 3 ? [down[down.length - 3], down[down.length - 2], down[down.length - 1]] : '',
+        上涨: 上涨.length > 3 ? [上涨[上涨.length - 3], 上涨[上涨.length - 2], 上涨[上涨.length - 1]] : '',
+        下跌: 下跌.length > 3 ? [下跌[下跌.length - 3], 下跌[下跌.length - 2], 下跌[下跌.length - 1]] : '',
+        追涨: 追涨.length > 3 ? [追涨[追涨.length - 3], 追涨[追涨.length - 2], 追涨[追涨.length - 1]] : '',
+        追跌: 追跌.length > 3 ? [追跌[追跌.length - 3], 追跌[追跌.length - 2], 追跌[追跌.length - 1]] : '',
         波动率: lastNumber(realData.dataExt[symbol].期货.波动率),
     })
 }
@@ -29,6 +33,27 @@ export const 信号灯side = (symbol: BaseType.BitmexSymbol) => {
     }
     else {
         return { 信号side: 'none' as 'none', 信号msg: '' }
+    }
+}
+
+const is连续3根全亮 = (arr: ArrayLike<{ value: boolean }[]>) =>
+    arr.length > 3 && arr[arr.length - 1].every(v => v.value) && arr[arr.length - 2].every(v => v.value) && arr[arr.length - 3].every(v => v.value)
+
+export const get信号灯Type = (symbol: BaseType.BitmexSymbol) => {
+
+    if (is连续3根全亮(realData.dataExt[symbol].期货.信号_上涨)) {
+        return '摸顶'
+    }
+    else if (is连续3根全亮(realData.dataExt[symbol].期货.信号_下跌)) {
+        return '抄底'
+    }
+    else if (is连续3根全亮(realData.dataExt[symbol].期货.信号_追涨)) {
+        return '追涨'
+    }
+    else if (is连续3根全亮(realData.dataExt[symbol].期货.信号_追跌)) {
+        return '追跌'
+    } else {
+        return 'none'
     }
 }
 
--- src/OrderServer/task/自动开仓step.ts
+++ src/OrderServer/task/自动开仓step.ts
@@ -1,73 +1,103 @@
 import { BaseType } from '../../lib/BaseType'
 import { TradeAccount } from '../TradeAccount'
-import { 信号灯side, realData, get波动率 } from '../realData'
+import { realData, get波动率, get信号msg, get信号灯Type } from '../realData'
 import { toBuySellPriceFunc } from '../../lib/C/toBuySellPriceFunc'
 import { sleep } from '../../lib/C/sleep'
 import { task__config } from './task__config'
 
 
-const 自动开仓step = (symbol: BaseType.BitmexSymbol) => async (self: TradeAccount) => {
+const 自动开仓step = (symbol: BaseType.BitmexSymbol) => {
 
-    if (self.jsonSync.rawData.symbol[symbol].任务开关.自动开仓.value === false) {
-        return true
-    }
+    let 最后一次信号 = 'none' as 'none' | '追涨' | '追跌' | '摸顶' | '抄底'
+    let 最后一次信号时间 = 0
 
-    const path = self.accountName + '.txt'
+    return async (self: TradeAccount) => {
 
-    const { 仓位数量 } = self.jsonSync.rawData.symbol[symbol]
+        if (self.jsonSync.rawData.symbol[symbol].任务开关.自动开仓.value === false) {
+            return true
+        }
 
-    const 本地维护仓位数量 = symbol === 'XBTUSD' ? self.ws.本地维护XBTUSD仓位数量 : self.ws.本地维护ETHUSD仓位数量
+        const path = self.accountName + '.txt'
 
-    const 连续止损次数 = symbol === 'XBTUSD' ? self.ws.连续止损XBTUSD : self.ws.连续止损ETHUSD
+        const { 仓位数量 } = self.jsonSync.rawData.symbol[symbol]
 
-    const 活动委托 = self.活动委托[symbol].filter(v => v.type !== '止损')
+        const 本地维护仓位数量 = symbol === 'XBTUSD' ? self.ws.本地维护XBTUSD仓位数量 : self.ws.本地维护ETHUSD仓位数量
 
-    const { 信号side, 信号msg } = 信号灯side(symbol)
+        const 连续止损次数 = symbol === 'XBTUSD' ? self.ws.连续止损XBTUSD : self.ws.连续止损ETHUSD
 
-    if (连续止损次数 >= 4) {
-        await sleep(1000 * 60 * 10)//10min
-        if (symbol === 'XBTUSD') self.ws.连续止损XBTUSD = 0
-        if (symbol === 'ETHUSD') self.ws.连续止损ETHUSD = 0
-    }
+        const 活动委托 = self.活动委托[symbol].filter(v => v.type !== '止损')
 
-    //没有仓位 没有委托 信号灯全亮 挂单
-    if (本地维护仓位数量 === 0 && 仓位数量 === 0 && 活动委托.length === 0 && 信号side !== 'none') {
-        return get波动率(symbol) < 30 ?
-            await self.order自动.taker({
-                symbol,
-                side: 信号side,
-                size: task__config.交易数量 * (连续止损次数 + 1),
-            }, { path, text: '自动开仓step 自动开仓 市价' + 信号msg }, self.ws) :
-            await self.order自动.limit({
-                symbol,
-                side: 信号side,
-                size: task__config.交易数量 * (连续止损次数 + 1),
-                price: toBuySellPriceFunc(信号side, () => realData.getOrderPrice({
+        const 信号灯Type = get信号灯Type(symbol)
+        const 开仓side = { '追涨': 'Buy', '追跌': 'Sell', '抄底': 'Buy', '摸顶': 'Sell', 'none': '_____' }[信号灯Type] as BaseType.Side
+
+        if (连续止损次数 >= 4) {
+            await sleep(1000 * 60 * 10)//10min
+            if (symbol === 'XBTUSD') self.ws.连续止损XBTUSD = 0
+            if (symbol === 'ETHUSD') self.ws.连续止损ETHUSD = 0
+        }
+
+
+        //开仓
+        if (本地维护仓位数量 === 0 && 仓位数量 === 0 && 活动委托.length === 0 && 信号灯Type !== 'none') {
+
+            const x = 最后一次信号
+            最后一次信号 = 信号灯Type
+            最后一次信号时间 = Date.now()
+
+            if (
+                (Date.now() - 最后一次信号时间 < 20 * 1000) || //没有超时
+                (                                             //抵消
+                    (x === '追涨' && 信号灯Type === '摸顶') ||
+                    (x === '追跌' && 信号灯Type === '抄底') ||
+                    (x === '抄底' && 信号灯Type === '追跌') ||
+                    (x === '摸顶' && 信号灯Type === '追涨')
+                )
+            ) {
+                return false
+            }
+
+
+
+            const 市价 = 信号灯Type === '追涨' || 信号灯Type === '追跌' || get波动率(symbol) < 30
+
+            return 市价 ?
+                await self.order自动.taker({
                     symbol,
-                    side: 信号side,
-                    type: 'taker',// 'maker',
-                    位置: 0,
-                })),
-                // reduceOnly: false,
-            }, { path, text: '自动开仓step 自动开仓 挂单' + 信号msg }, self.ws)
-    }
+                    side: 开仓side,
+                    size: task__config.交易数量 * (连续止损次数 + 1),
+                }, { path, text: '自动开仓step 自动开仓 市价' + get信号msg(symbol) }, self.ws) :
+                await self.order自动.limit({
+                    symbol,
+                    side: 开仓side,
+                    size: task__config.交易数量 * (连续止损次数 + 1),
+                    price: toBuySellPriceFunc(开仓side, () => realData.getOrderPrice({
+                        symbol,
+                        side: 开仓side,
+                        type: 'taker',
+                        位置: 0,
+                    })),
+                }, { path, text: '自动开仓step 自动开仓 挂单' + get信号msg(symbol) }, self.ws)
+        }
 
-    //有开仓单(限价)  
-    if (活动委托.length === 1) {
-        const { type, timestamp, id, side } = 活动委托[0]
-        if (type === '限价') {
-            const _15秒取消 = (Date.now() > (timestamp + 15 * 1000))
-            const 出现反向信号时候取消 = (信号side !== 'none' && 信号side !== side)
-            if (_15秒取消 || 出现反向信号时候取消) {
-                return await self.order自动.cancel([id], { path, text: '自动开仓step 取消开仓 ' + _15秒取消 ? '_15秒取消' : ('出现反向信号时候取消' + 信号msg) })
+
+
+        //取消开仓
+        if (活动委托.length === 1) {
+            const { type, timestamp, id, side } = 活动委托[0]
+            if (type === '限价') {
+                const _15秒取消 = (Date.now() > (timestamp + 15 * 1000))
+                const 出现反向信号时候取消 = (信号灯Type !== 'none' && 开仓side !== side)
+                if (_15秒取消 || 出现反向信号时候取消) {
+                    return await self.order自动.cancel([id], { path, text: '自动开仓step 取消开仓 ' + _15秒取消 ? '_15秒取消' : ('出现反向信号时候取消' + get信号msg(symbol)) })
+                }
             }
         }
+
+
+        return false
     }
 
-
-    return false
 }
-
 
 
 export const XBTUSD自动开仓step = 自动开仓step('XBTUSD') 
