58714276f10fac95a7b518e9f4267a05424c67c6
fc01 <910677221@qq.com>
2019-02-23 16:30:48 +0800

    is交叉

--- src/RealDataServer/指标.ts
+++ src/RealDataServer/指标.ts
@@ -4,9 +4,9 @@ export namespace 指标 {
 
     export const is交叉 = ({ a1, a2, b1, b2 }: { a1: number, a2: number, b1: number, b2: number }) => {
         if (isNaN(a1) || isNaN(a2) || isNaN(b1) || isNaN(b2)) return false
-    
+
         const 没有交叉 = (a1 > b1 && a2 > b2) || (a1 < b1 && a2 < b2)
-    
+
         return !没有交叉
     }
 
