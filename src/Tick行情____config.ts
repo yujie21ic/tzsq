215284c7811734cc5b60223b8ff25596f38c884d
schu <workfor2012@gmail.com>
2019-02-25 00:03:55 +0800

    替换买卖5为买卖1

--- src/Tick行情____config.ts
+++ src/Tick行情____config.ts
@@ -580,7 +580,7 @@ export const Tick行情____config: { [key in string]: ItemFunc } = {
             },
         ]
     }),
-    测试盘口: (d, d2) => ({
+    测试盘口上涨: (d, d2) => ({
         heightList: [0.4, 0.2, 0.2, 0.2],
         items: [
             [
@@ -644,7 +644,76 @@ export const Tick行情____config: { [key in string]: ItemFunc } = {
             },
             {
                 layerList: [
-                    layer(信号Layer, { data: d.期货.信号_上涨, color: 卖颜色 }),
+                    layer(信号Layer, { data: d.期货.信号_上涨1, color: 卖颜色 }),
+                ]
+            },
+        ]
+    }),
+    测试盘口下跌: (d, d2) => ({
+        heightList: [0.4, 0.2, 0.2, 0.2],
+        items: [
+            [
+            // {
+            //     layerList: [净成交量均线30
+            //         layer(LineLayer, { data: d2.XBTUSD.hopex.价格, color: ETH颜色 }),
+            //     ]
+            // },
+            // {
+            
+            {
+                
+                numberColor: 波动率颜色,
+                numberX: 100,
+                layerList: [
+                    layer(ZeroLayer, { color: ETH颜色 }),
+                    layer(LineLayer, { data: d.期货.净成交量均线30, color: ETH颜色 }),
+                ]
+            },
+            {
+                numberColor: 石青,
+                numberX: 100,
+                layerList: [
+                    layer(LineLayer, { data: d.期货.波动率, color: 石青 }),
+                ]
+            },
+            {
+                numberColor: BTC颜色,
+                layerList: [
+                    layer(LineLayer, { data: d2.XBTUSD.期货.价格, color: BTC颜色 }),
+
+                    layer(TextLayer, {
+                        text:
+                            `hopex:${lastNumber(d2.XBTUSD.hopex.价格).toFixed(2)}  ` +
+                            `bitmex:${lastNumber(d.期货.价格).toFixed(2)}      ` +
+                            `期货30秒内成交量:${d.期货30秒内成交量().toFixed(2)}万   ` +
+                            `期货波动率:${lastNumber(d.期货.波动率).toFixed(2)}`,
+                        color: d === d2.XBTUSD ? BTC颜色 : ETH颜色,
+                    })
+
+                ]
+            },
+            ],
+          
+
+            {
+                layerList: [
+                    layer(ZeroLayer, { color: 0xaaaaaa }),
+                    layer(LineLayer, { data: d.期货.盘口买1, color: 买颜色 }),
+                    layer(LineLayer, { data: d.期货.盘口卖1, color: 卖颜色, 临时参数: '变成负数' }),
+                    layer(LineLayer, { data: d.期货.净盘口1, color: BTC颜色 }),
+                    layer(LineLayer, { data: d.期货.净盘口均线1, color: ETH颜色 }),
+                ]
+            },
+            {
+                layerList: [
+                    layer(ZeroLayer, { color: 0xaaaaaa }),
+                    layer(LineLayer, { data: d.期货.净上涨成交量DIF, color: 买颜色1 }),
+                    layer(LineLayer, { data: d.期货.净上涨成交量DEM, color: 买颜色 }),
+                ]
+            },
+            {
+                layerList: [
+                    layer(信号Layer, { data: d.期货.信号_下跌1, color: 买颜色 }),
                 ]
             },
         ]
