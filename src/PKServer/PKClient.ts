744651fa9522fac34d2834dd312558b4b5881079
schu <workfor2012@gmail.com>
2019-02-26 00:58:04 +0800

    更新盘口服务器

--- src/PKServer/PKClient.ts
+++ src/PKServer/PKClient.ts
@@ -4,6 +4,6 @@ import { funcList } from './funcList'
 export const PKClient = new JSONRPCClient({
     funcList,
     //盘口服务器
-    host: '34.254.250.43',
+    host: '34.247.214.96',
     port: 5555
 })
