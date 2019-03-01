a9d4382b5e4179824d22dbaad0015bd496cee03a
fc01 <910677221@qq.com>
2019-03-02 02:08:03 +0800

    ——

--- src/OrderServer/index.ts
+++ src/OrderServer/index.ts
@@ -11,6 +11,8 @@ import { 委托检测step } from './task/委托检测step'
 import { XBTUSD自动开仓step } from './task/自动开仓step'
 import { XBTUSD自动止盈波段step } from './task/自动止盈波段step'
 import { XBTUSD自动止盈step } from './task/自动止盈step'
+import { to范围 } from '../lib/F/to范围';
+import { toBuySellPriceFunc } from '../lib/C/toBuySellPriceFunc'
 // import { 哪边多挂哪边 } from './task/哪边多挂哪边'
 
 
@@ -97,7 +99,87 @@ server.func.市价平仓 = async req => {
 server.func.下单 = async req => {
     const account = accountDic.get(req.cookie)
     if (account === undefined) throw 'cookie不存在'
-    return account.下单(req)
+
+    if (req.symbol !== 'XBTUSD' && req.symbol !== 'ETHUSD') {
+        throw 'symbol不存在'
+    }
+
+    const getOrderPrice = () =>
+        TradeAccount.realData.getOrderPrice({
+            symbol: req.symbol,
+            side: req.side,
+            type: req.type,
+            位置: Math.floor(to范围({ min: 0, max: 4, value: req.位置 }))
+        })
+
+    const getPrice = req.最低_最高 ?
+        () => {
+            const price = getOrderPrice()
+            const { high, low } = TradeAccount.realData.get期货多少秒内最高最低(req.symbol, 5)
+            if (req.side === 'Buy') {
+                return Math.min(price, low)
+            } else {
+                return Math.max(price, high)
+            }
+        } :
+        getOrderPrice
+
+    if (req.type === 'maker' && isNaN(getPrice())) {
+        throw '服务器还没有 买1 卖1 价格'
+    }
+
+    const { 仓位数量 } = account.jsonSync.rawData.symbol[req.symbol]
+
+    const 活动委托 = account.活动委托[req.symbol].filter(v => v.type !== '止损')
+
+    if (活动委托.length > 1) {
+        throw '已经有委托了'
+    }
+    else if (活动委托.length === 1) {
+        //更新 限价委托
+        if (
+            (
+                活动委托[0].type === '限价' ||
+                活动委托[0].type === '限价只减仓'
+            )
+            && 活动委托[0].side === req.side && req.type === 'maker') {
+            //ws返回有时间  直接给委托列表加一条记录??
+            return await account.updateMaker({
+                orderID: 活动委托[0].id,
+                price: toBuySellPriceFunc(req.side, getPrice),
+                text: '手动updateMaker'
+            })
+        } else {
+            throw '已经有委托了'
+        }
+    }
+
+
+    if ((仓位数量 > 0 && req.side !== 'Sell') ||
+        (仓位数量 < 0 && req.side !== 'Buy')
+    ) {
+        throw '不能加仓'
+    }
+
+    return req.type === 'taker' ?
+        (req.最低_最高 ?
+            false :
+            await account.taker({
+                symbol: req.symbol,
+                side: req.side,
+                size: req.size,
+                text: '手动taker',
+            })
+        ) :
+        //ws返回有时间  直接给委托列表加一条记录??
+        await account.maker({
+            symbol: req.symbol,
+            side: req.side,
+            size: req.size,
+            price: toBuySellPriceFunc(req.side, getPrice),
+            reduceOnly: 仓位数量 !== 0,
+            text: '手动maker',
+        })
 }
 
 server.func.任务_开关 = async req => {
--- src/统一接口/TradeAccount.ts
+++ src/统一接口/TradeAccount.ts
@@ -1,8 +1,5 @@
-import { funcList } from '../OrderServer/____API____'
 import { BaseType } from '../lib/BaseType'
 import { sleep } from '../lib/C/sleep'
-import { to范围 } from '../lib/F/to范围'
-import { toBuySellPriceFunc } from '../lib/C/toBuySellPriceFunc'
 import { BitmexPositionAndOrder } from './PositionAndOrder/BitmexPositionAndOrder'
 import { RealData__Server } from '../RealDataServer/RealData__Server'
 import { RealDataBase } from '../RealDataServer/RealDataBase'
@@ -34,92 +31,6 @@ export class TradeAccount extends BitmexPositionAndOrder {
         } else {
             return 0
         }
-    }
-
-
-
-    下单 = async (req: typeof funcList.下单.req) => {
-
-        if (req.symbol !== 'XBTUSD' && req.symbol !== 'ETHUSD') {
-            throw 'symbol不存在'
-        }
-
-        const getOrderPrice = () =>
-            TradeAccount.realData.getOrderPrice({
-                symbol: req.symbol,
-                side: req.side,
-                type: req.type,
-                位置: Math.floor(to范围({ min: 0, max: 4, value: req.位置 }))
-            })
-
-        const getPrice = req.最低_最高 ?
-            () => {
-                const price = getOrderPrice()
-                const { high, low } = TradeAccount.realData.get期货多少秒内最高最低(req.symbol, 5)
-                if (req.side === 'Buy') {
-                    return Math.min(price, low)
-                } else {
-                    return Math.max(price, high)
-                }
-            } :
-            getOrderPrice
-
-        if (req.type === 'maker' && isNaN(getPrice())) {
-            throw '服务器还没有 买1 卖1 价格'
-        }
-
-        const { 仓位数量 } = this.jsonSync.rawData.symbol[req.symbol]
-
-        const 活动委托 = this.活动委托[req.symbol].filter(v => v.type !== '止损')
-
-        if (活动委托.length > 1) {
-            throw '已经有委托了'
-        }
-        else if (活动委托.length === 1) {
-            //更新 限价委托
-            if (
-                (
-                    活动委托[0].type === '限价' ||
-                    活动委托[0].type === '限价只减仓'
-                )
-                && 活动委托[0].side === req.side && req.type === 'maker') {
-                //ws返回有时间  直接给委托列表加一条记录??
-                return await this.updateMaker({
-                    orderID: 活动委托[0].id,
-                    price: toBuySellPriceFunc(req.side, getPrice),
-                    text: '手动updateMaker'
-                })
-            } else {
-                throw '已经有委托了'
-            }
-        }
-
-
-        if ((仓位数量 > 0 && req.side !== 'Sell') ||
-            (仓位数量 < 0 && req.side !== 'Buy')
-        ) {
-            throw '不能加仓'
-        }
-
-        return req.type === 'taker' ?
-            (req.最低_最高 ?
-                false :
-                await this.taker({
-                    symbol: req.symbol,
-                    side: req.side,
-                    size: req.size,
-                    text: '手动taker',
-                })
-            ) :
-            //ws返回有时间  直接给委托列表加一条记录??
-            await this.maker({
-                symbol: req.symbol,
-                side: req.side,
-                size: req.size,
-                price: toBuySellPriceFunc(req.side, getPrice),
-                reduceOnly: 仓位数量 !== 0,
-                text: '手动maker',
-            })
     }
 
 }
