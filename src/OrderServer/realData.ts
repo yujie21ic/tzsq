224a225d0cf3295e7e6294c284b4e7be11a00435
fc01 <910677221@qq.com>
2019-03-02 00:04:53 +0800

    is连续几根全亮

--- src/OrderServer/realData.ts
+++ src/OrderServer/realData.ts
@@ -2,28 +2,9 @@ import { RealData } from '../RealDataServer/RealData'
 export const realData = new RealData(false)
 import { BaseType } from '../lib/BaseType'
 import { lastNumber } from '../lib/F/lastNumber'
+import { is连续几根全亮 } from '../lib/C/is连续几根全亮'
 
 //_________________________________________________________________________//
-
-
-
-
-
-
-const is连续几根全亮 = (几根: number, arr: ArrayLike<{ value: boolean }[]>) => {
-    let 连续几根 = 0
-    for (let i = arr.length - 1; i >= 0; i--) {
-        if (arr[i].every(v => v.value)) {
-            连续几根++
-            if (连续几根 === 几根) return true
-        } else {
-            return false
-        }
-    }
-    return false
-}
-
-
 
 export const get信号灯Type = (symbol: BaseType.BitmexSymbol) => {
 
