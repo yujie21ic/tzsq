13961521ffda9e7cf4f84709bf573d50bc74bf14
fc01 <910677221@qq.com>
2019-03-01 12:53:10 +0800

    ____

--- src/OrderServer/task/自动止盈波段step.ts
+++ src/OrderServer/task/自动止盈波段step.ts
@@ -36,19 +36,19 @@ const 自动止盈波段step = (symbol: BaseType.BitmexSymbol) => {
 
             if (活动委托.length <= 1) {
 
-                const side = 仓位数量 > 0 ? 'Sell' : 'Buy'
+                const 平仓side = 仓位数量 > 0 ? 'Sell' : 'Buy'
 
                 const getPrice = () => {
                     const 止盈点 = get波动率(symbol) / 3 + 3
-                    const 止盈点价格 = toGridPoint(symbol, 仓位数量 > 0 ? 开仓均价 + 止盈点 : 开仓均价 - 止盈点, side)
+                    const 止盈点价格 = toGridPoint(symbol, 仓位数量 > 0 ? 开仓均价 + 止盈点 : 开仓均价 - 止盈点, 平仓side)
 
                     const 位置1价格 = realData.getOrderPrice({
                         symbol,
-                        side,
+                        side: 平仓side,
                         type: 'maker',
                         位置: 0,
                     })
-                    return side === 'Buy' ?
+                    return 平仓side === 'Buy' ?
                         Math.min(止盈点价格, 位置1价格) :
                         Math.max(止盈点价格, 位置1价格)
                 }
@@ -60,7 +60,7 @@ const 自动止盈波段step = (symbol: BaseType.BitmexSymbol) => {
 
                 const get位置1价格 = () => realData.getOrderPrice({
                     symbol,
-                    side,
+                    side: 平仓side,
                     type: 'maker',
                     位置: 0,
                 })
@@ -70,29 +70,42 @@ const 自动止盈波段step = (symbol: BaseType.BitmexSymbol) => {
                 const 最后一次开仓type = self.ws.增量同步数据.最后一次自动开仓.get(symbol)
                 let 亏损挂单平仓Text = ''
 
-                //下单30s后，折返没有超过下单点的折返函数，挂单全平 
-                if ((最后一次开仓type === '摸顶' || 最后一次开仓type === '抄底') &&
-                    Date.now() - 最后一次开仓时间 >= 摸顶抄底超时秒 * 1000 &&
-                    self.get浮盈点数(symbol) < 最后一次开仓折返率) {
-                    亏损挂单平仓Text = '下单' + 摸顶抄底超时秒 + ' 秒后，折返没有超过下单点的折返函数，挂单全平'
+
+                if (最后一次开仓type === '摸顶' || 最后一次开仓type === '抄底') {
+                    if (
+                        Date.now() - 最后一次开仓时间 >= 摸顶抄底超时秒 * 1000 &&
+                        self.get浮盈点数(symbol) < 最后一次开仓折返率
+                    ) {
+                        亏损挂单平仓Text = '下单' + 摸顶抄底超时秒 + ' 秒后，折返没有超过下单点的折返函数，挂单全平'
+                    }
+                }
+                else if (最后一次开仓type === '追涨' || 最后一次开仓type === '追跌') {
+                    const 净成交量 = lastNumber(realData.dataExt[symbol].期货.净成交量)
+
+                    if ((平仓side === 'Sell' && 净成交量 < 0) || (平仓side === 'Buy' && 净成交量 > 0)) {
+                        亏损挂单平仓Text = '如果净成交量反向，那么立刻挂单平仓'
+                    } 
+                    else if (Date.now() - 最后一次开仓时间 > 5 * 60 * 1000) {
+                        亏损挂单平仓Text = '如果净成交量没有反向，那么最多等待5分钟，然后挂单平仓'
+                    } else {
+                        //如果遇到极值点，平仓只需要两根信号
+                    }
+
                 }
 
-                //追涨追跌如果5分钟还没有涨超过10点，挂单全平
-                if ((最后一次开仓type === '追涨' || 最后一次开仓type === '追跌') &&
-                    Date.now() - 最后一次开仓时间 >= 5 * 60 * 1000 && self.get浮盈点数(symbol) <= 10) {
-                    亏损挂单平仓Text = '自动止盈波段step 追涨追跌如果5分钟还没有涨超过10点，挂单全平'
-                }
+
+
 
 
                 if (亏损挂单平仓Text !== '') {
 
                     if (活动委托.length === 1) {
-                        if (活动委托[0].type === '限价只减仓' && 活动委托[0].side === side) {
+                        if (活动委托[0].type === '限价只减仓' && 活动委托[0].side === 平仓side) {
                             if (活动委托[0].price !== get位置1价格()) {
                                 return await self.order自动.updateMaker({
                                     orderID: 活动委托[0].id,
-                                    price: toBuySellPriceFunc(side, get位置1价格),
-                                    text: 亏损挂单平仓Text + '  updateMaker'
+                                    price: toBuySellPriceFunc(平仓side, get位置1价格),
+                                    text: 最后一次开仓type + '平仓' + '  ' + 亏损挂单平仓Text + '  重新挂' + 平仓side + '1'
                                 }, '', self.ws)
                             } else {
                                 return false
@@ -101,11 +114,11 @@ const 自动止盈波段step = (symbol: BaseType.BitmexSymbol) => {
                     } else {
                         return await self.order自动.maker({
                             symbol,
-                            side,
+                            side: 平仓side,
                             size: 最大仓位abs,
-                            price: toBuySellPriceFunc(side, get位置1价格),
+                            price: toBuySellPriceFunc(平仓side, get位置1价格),
                             reduceOnly: true,
-                            text: 亏损挂单平仓Text,
+                            text: 最后一次开仓type + '平仓' + '  ' + 亏损挂单平仓Text,
                         }, '', self.ws)
                     }
                 }
@@ -115,11 +128,11 @@ const 自动止盈波段step = (symbol: BaseType.BitmexSymbol) => {
                 if (self.ws.增量同步数据.最后一次自动开仓.get(symbol) === '摸顶' && is上涨做空下跌平仓(symbol) && 活动委托.length === 0) {
                     return await self.order自动.maker({
                         symbol,
-                        side,
+                        side: 平仓side,
                         size: Math.round(最大仓位abs / 2),//一半
-                        price: toBuySellPriceFunc(side, get位置1价格),
+                        price: toBuySellPriceFunc(平仓side, get位置1价格),
                         reduceOnly: true,
-                        text: '自动止盈波段step 上涨做空下跌平仓',
+                        text: 最后一次开仓type + '平仓' + '  ' + '自动止盈波段step 上涨做空下跌平仓一半',
                     }, get信号XXXmsg(symbol), self.ws)
                 }
 
@@ -127,11 +140,11 @@ const 自动止盈波段step = (symbol: BaseType.BitmexSymbol) => {
                 if (self.ws.增量同步数据.最后一次自动开仓.get(symbol) === '抄底' && is下跌抄底上涨平仓(symbol) && 活动委托.length === 0) {
                     return await self.order自动.maker({
                         symbol,
-                        side,
+                        side: 平仓side,
                         size: Math.round(最大仓位abs / 2),//一半
-                        price: toBuySellPriceFunc(side, get位置1价格),
+                        price: toBuySellPriceFunc(平仓side, get位置1价格),
                         reduceOnly: true,
-                        text: '自动止盈波段step 下跌抄底上涨平仓',
+                        text: 最后一次开仓type + '平仓' + '  ' + '自动止盈波段step 下跌抄底上涨平仓一半',
                     }, get信号XXXmsg(symbol), self.ws)
                 }
 
@@ -140,15 +153,15 @@ const 自动止盈波段step = (symbol: BaseType.BitmexSymbol) => {
                 //触发了反向开仓信号 
                 const { 信号side, 信号msg } = 摸顶抄底信号灯side(symbol)
 
-                if (信号side === side && 活动委托.length === 0) {
+                if (信号side === 平仓side && 活动委托.length === 0) {
 
                     return await self.order自动.maker({
                         symbol,
-                        side,
+                        side: 平仓side,
                         size: Math.round(最大仓位abs / 2),//一半
-                        price: toBuySellPriceFunc(side, get位置1价格),
+                        price: toBuySellPriceFunc(平仓side, get位置1价格),
                         reduceOnly: true,
-                        text: '自动止盈波段step 平一半',
+                        text: 最后一次开仓type + '平仓' + '  ' + '自动止盈波段step 平一半',
                     }, 信号side + ' 信号msg:' + 信号msg, self.ws)
 
                     // if (
