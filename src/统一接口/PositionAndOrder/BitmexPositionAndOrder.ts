f5986b0123c1ef07681210bdb8c30bc8b9de7df8
fc01 <910677221@qq.com>
2019-03-02 01:49:58 +0800

    BitmexPositionAndOrder

--- src/统一接口/PositionAndOrder/BitmexPositionAndOrder.ts
+++ src/统一接口/PositionAndOrder/BitmexPositionAndOrder.ts
@@ -1,6 +1,373 @@
-import { BitMEXOrderAPI } from '../BitMEX/BitMEXOrderAPI'
+import { BaseType } from '../../lib/BaseType'
+import { BitMEXRESTAPI } from '../BitMEX/BitMEXRESTAPI'
+import { sleep } from '../../lib/C/sleep'
+import { JSONRequestError } from '../../lib/C/JSONRequest'
+import { logToFile } from '../../lib/C/logToFile'
+import { keys } from 'ramda'
+import { JSONSync } from '../../lib/C/JSONSync'
+import { BitMEXWSAPI } from '../BitMEX/BitMEXWSAPI'
 
+const symbol = () => ({
+    任务开关: {
+        自动开仓摸顶: {
+            value: false,
+            text: '',
+        },
+        自动开仓抄底: {
+            value: false,
+            text: '',
+        },
+        自动开仓追涨: {
+            value: false,
+            text: '',
+        },
+        自动开仓追跌: {
+            value: false,
+            text: '',
+        },
+        自动止盈: {
+            value: false,
+            text: '',
+        },
+        自动止盈波段: {
+            value: false,
+            text: '',
+        },
+        自动推止损: {
+            value: true,
+            text: '',
+        }
+    },
 
-export class BitmexPositionAndOrder extends BitMEXOrderAPI {
- 
+    委托: {
+        id: '',
+        side: '' as BaseType.Side,
+        cumQty: 0,      //成交数量
+        orderQty: 0,    //委托数量
+        price: 0,
+    },
+    止损价格: 0,
+    仓位数量: 0,
+    开仓均价: 0,
+})
+
+export const createJSONSync = () =>
+    new JSONSync({
+        symbol: {
+            XBTUSD: symbol(),
+            ETHUSD: symbol(),
+        }
+    })
+
+type Order = {
+    type: '限价' | '限价只减仓' | '止损'
+    timestamp: number
+    id: string
+    side: BaseType.Side
+    cumQty: number      //成交数量
+    orderQty: number    //委托数量
+    price: number
 }
+
+let callID = 0
+
+const 重试几次 = 10
+const 重试休息多少毫秒 = 10
+
+export class BitmexPositionAndOrder {
+
+    private cookie: string
+    log = (text: string) => { }
+    ws: BitMEXWSAPI
+    jsonSync = createJSONSync()
+    活动委托 = {
+        XBTUSD: [] as Order[],
+        ETHUSD: [] as Order[],
+    }
+
+    accountName: string
+
+    constructor(p: { accountName: string, cookie: string }) {
+        this.accountName = p.accountName
+        this.cookie = p.cookie
+
+        this.ws = new BitMEXWSAPI(p.cookie, [
+            { theme: 'position' },
+            { theme: 'order' },
+        ])
+
+        this.log = logToFile(p.accountName + '.txt')
+        this.ws.增量同步数据.log = logToFile(p.accountName + '.txt')
+
+        this.ws.onmessage = frame => {
+
+            if (frame.table === 'position') {
+                this.updatePosition()
+            }
+            else if (frame.table === 'order') {
+                this.updateOrder()
+            }
+        }
+    }
+
+
+
+    private updatePosition() {
+        keys(this.jsonSync.rawData.symbol).forEach(symbol => {
+            this.ws.data.position.forEach(item => {
+                if (item.symbol === symbol) {
+                    const { 仓位数量, 开仓均价 } = this.jsonSync.data.symbol[symbol]
+                    const raw = this.jsonSync.rawData.symbol[symbol]
+                    if (item !== undefined) {
+                        if (raw.仓位数量 !== item.currentQty || raw.开仓均价 !== item.avgCostPrice) {
+                            仓位数量.____set(item.currentQty)
+                            开仓均价.____set(item.avgCostPrice)
+                            this.log(`仓位更新: ${symbol} 仓位数量:${item.currentQty}  本地维护仓位数量:${this.ws.增量同步数据.仓位数量.get(symbol)}  开仓均价:${item.avgCostPrice}`)
+                        }
+                    } else {
+                        if (raw.仓位数量 !== 0 || raw.开仓均价 !== 0) {
+                            仓位数量.____set(0)
+                            开仓均价.____set(0)
+                            this.log(`仓位更新: ${symbol} 仓位数量:0  本地维护仓位数量:${this.ws.增量同步数据.仓位数量.get(symbol)}`)
+                        }
+                    }
+                }
+            })
+
+        })
+    }
+
+    private updateOrder() {
+
+        keys(this.jsonSync.rawData.symbol).forEach(symbol => {
+
+            const arr = [] as {
+                type: '限价' | '限价只减仓' | '止损'
+                timestamp: number
+                id: string
+                side: BaseType.Side
+                cumQty: number
+                orderQty: number
+                price: number
+            }[]
+
+            this.ws.data.order.forEach(v => {
+                if (v.symbol === symbol) {
+                    if (v.ordType === 'Limit' && v.execInst === 'ParticipateDoNotInitiate,ReduceOnly' && v.workingIndicator) {//先检测只减仓
+                        arr.push({
+                            type: '限价只减仓',
+                            timestamp: new Date(v.timestamp).getTime(),
+                            id: v.orderID,
+                            side: v.side as BaseType.Side,
+                            cumQty: v.cumQty,
+                            orderQty: v.orderQty,
+                            price: v.price,
+                        })
+                    }
+                    else if (v.ordType === 'Limit' /*&& v.execInst === 'ParticipateDoNotInitiate'*/ && v.workingIndicator) { //不勾被动委托也行
+                        arr.push({
+                            type: '限价',
+                            timestamp: new Date(v.timestamp).getTime(),
+                            id: v.orderID,
+                            side: v.side as BaseType.Side,
+                            cumQty: v.cumQty,
+                            orderQty: v.orderQty,
+                            price: v.price,
+                        })
+                    }
+                    else if (v.ordType === 'Stop' && v.execInst === 'Close,LastPrice') {
+                        arr.push({
+                            type: '止损',
+                            timestamp: new Date(v.timestamp).getTime(),
+                            id: v.orderID,
+                            side: v.side as BaseType.Side,
+                            cumQty: v.cumQty,
+                            orderQty: v.orderQty,
+                            price: v.stopPx,
+                        })
+                    }
+                }
+            })
+
+
+            this.活动委托[symbol] = arr
+
+            const x = this.活动委托[symbol].find(v => v.type !== '止损')
+            if (x === undefined) {
+                this.jsonSync.data.symbol[symbol].委托.id.____set('')
+            } else {
+                this.jsonSync.data.symbol[symbol].委托.cumQty.____set(x.cumQty)
+                this.jsonSync.data.symbol[symbol].委托.id.____set(x.id)
+                this.jsonSync.data.symbol[symbol].委托.orderQty.____set(x.orderQty)
+                this.jsonSync.data.symbol[symbol].委托.price.____set(x.price)
+                this.jsonSync.data.symbol[symbol].委托.side.____set(x.side)
+            }
+
+            const y = this.活动委托[symbol].find(v => v.type === '止损')
+            if (y === undefined) {
+                this.jsonSync.data.symbol[symbol].止损价格.____set(0)
+            } else {
+                this.jsonSync.data.symbol[symbol].止损价格.____set(y.price)
+            }
+        })
+    }
+
+    private DDOS调用 = <P extends { text: string }>(f: (cookie: string, p: P) => Promise<{ error?: JSONRequestError, data?: any }>) =>
+        async (p: P, logText = '') => {
+            const startTime = Date.now()
+            let success = false
+            let i = 1
+            let errMsg = ''
+            const __id__ = callID++
+
+            this.log(`__${__id__}__` + p.text + '  ' + logText + '\nsend:' + JSON.stringify(p))
+
+            for (i = 1; i <= 重试几次; i++) {
+                const ret = await f(this.cookie, p)
+
+                if (ret.error === '网络错误') {
+                    success = false
+                    errMsg = JSON.stringify(ret)
+                    break
+                }
+                else if (ret.error === undefined && ret.data !== undefined && ret.data.ordStatus !== 'Canceled' && ret.data.ordStatus !== 'Rejected') {
+                    success = true
+
+                    //
+                    if (this.ws !== undefined) {
+                        if (ret.data.orderID !== undefined && ret.data.ordStatus !== undefined) {
+                            this.ws.onAction({
+                                action: 'insert',
+                                table: 'order',
+                                data: [ret.data as any],
+                            })
+                        }
+                    }
+                    //
+
+                    break
+                }
+                await sleep(重试休息多少毫秒)
+                if (i === 重试几次) errMsg = JSON.stringify(ret)
+            }
+
+            this.log(`__${__id__}__` + `  重试${i}次  ${success ? '成功' : '失败' + errMsg}  耗时:${Date.now() - startTime}ms`)
+
+            if (success === false) {
+                await sleep(500)
+            }
+            return success
+        }
+
+
+    maker = this.DDOS调用<{
+        symbol: BaseType.BitmexSymbol
+        side: BaseType.Side
+        size: number
+        price: () => number
+        reduceOnly: boolean
+        text: string
+    }>(
+        (cookie, p) => BitMEXRESTAPI.Order.new(cookie, {
+            symbol: p.symbol,
+            ordType: 'Limit',
+            side: p.side,
+            orderQty: p.size,
+            price: p.price(),
+            execInst: p.reduceOnly ? 'ParticipateDoNotInitiate,ReduceOnly' : 'ParticipateDoNotInitiate',
+            text: p.text,
+        })
+    )
+
+    stop = this.DDOS调用<{
+        symbol: BaseType.BitmexSymbol
+        side: BaseType.Side
+        price: number
+        text: string
+    }>(
+        (cookie, p) => BitMEXRESTAPI.Order.new(cookie, {
+            symbol: p.symbol,
+            ordType: 'Stop',
+            stopPx: p.price,
+            orderQty: 100000,
+            side: p.side,
+            execInst: 'Close,LastPrice',
+            text: p.text,
+        })
+    )
+
+    updateStop = this.DDOS调用<{
+        orderID: string
+        price: number
+        text: string
+    }>(
+        (cookie, p) => BitMEXRESTAPI.Order.amend(cookie, {
+            orderID: p.orderID,
+            stopPx: p.price,
+            text: p.text,
+        })
+    )
+
+    updateMaker = this.DDOS调用<{
+        orderID: string
+        price: () => number
+        text: string
+    }>(
+        (cookie, p) => BitMEXRESTAPI.Order.amend(cookie, {
+            orderID: p.orderID,
+            price: p.price(),
+            text: p.text,
+        })
+    )
+
+    limit = this.DDOS调用<{
+        symbol: BaseType.BitmexSymbol
+        side: BaseType.Side
+        size: number
+        price: () => number
+        text: string
+    }>(
+        (cookie, p) => BitMEXRESTAPI.Order.new(cookie, {
+            symbol: p.symbol,
+            ordType: 'Limit',
+            side: p.side,
+            orderQty: p.size,
+            price: p.price() + ((p.side === 'Buy' ? 1 : -1) * (p.symbol === 'XBTUSD' ? 0.5 : 0.05)),
+            text: p.text,
+        })
+    )
+
+    taker = this.DDOS调用<{
+        symbol: BaseType.BitmexSymbol
+        side: BaseType.Side
+        size: number
+        text: string
+    }>(
+        (cookie, p) => BitMEXRESTAPI.Order.new(cookie, {
+            symbol: p.symbol,
+            ordType: 'Market',
+            side: p.side,
+            orderQty: p.size,
+            text: p.text,
+        })
+    )
+
+    close = this.DDOS调用<{
+        symbol: BaseType.BitmexSymbol
+        text: string
+    }>(
+        (cookie, p) => BitMEXRESTAPI.Order.new(cookie, {
+            symbol: p.symbol,
+            ordType: 'Market',
+            execInst: 'Close',
+            text: p.text,
+        })
+    )
+
+    cancel = this.DDOS调用<{
+        orderID: string[]
+        text: string
+    }>(
+        (cookie, p) => BitMEXRESTAPI.Order.cancel(cookie, { orderID: JSON.stringify(p.orderID), text: p.text })
+    )
+} 
