8743b30318f76fcfcfe0dc84bbbfce42ae825caf
fc01 <910677221@qq.com>
2019-03-02 02:08:51 +0800

    ——

--- src/OrderServer/index.ts
+++ src/OrderServer/index.ts
@@ -11,7 +11,7 @@ import { 委托检测step } from './task/委托检测step'
 import { XBTUSD自动开仓step } from './task/自动开仓step'
 import { XBTUSD自动止盈波段step } from './task/自动止盈波段step'
 import { XBTUSD自动止盈step } from './task/自动止盈step'
-import { to范围 } from '../lib/F/to范围';
+import { to范围 } from '../lib/F/to范围'
 import { toBuySellPriceFunc } from '../lib/C/toBuySellPriceFunc'
 // import { 哪边多挂哪边 } from './task/哪边多挂哪边'
 
