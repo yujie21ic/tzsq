445084721d809d67cb214062d0b65a0e32687cb8
fc01 <910677221@qq.com>
2019-03-03 15:45:24 +0800

    PKServer.run()

---
+++ src/DBServer/DBServer.ts
@@ -0,0 +1,27 @@
+import * as Sequelize from 'sequelize'
+import { JSONRPCServer } from '../lib/C/JSONRPC'
+import { BaseType } from '../lib/BaseType'
+import { funcList } from './funcList'
+import { DB } from './DB'
+import { timeID } from '../lib/F/timeID'
+
+export const DBServer = new JSONRPCServer({
+    funcList,
+    port: 5555
+})
+
+DBServer.func.getKLine = async req => {
+    const timeFunc = req.type === '1m' ? timeID.timestampToOneMinuteID : timeID.timestampTo500msID
+    const retData = await DB.getKLine(req.type, req.symbol).findAll<{}>({
+        raw: true,
+        where: {
+            id: {
+                [Sequelize.Op.gte]: timeFunc(req.startTime),
+                [Sequelize.Op.lte]: timeFunc(req.endTime),
+            }
+        },
+        order: ['id'],
+    }) as BaseType.KLine[]
+    return retData
+}
+
--- src/PKServer/index.ts
+++ src/PKServer/index.ts
@@ -1,76 +1,8 @@
-import * as Sequelize from 'sequelize'
-import { JSONRPCServer } from '../lib/C/JSONRPC'
-import { funcList } from './funcList'
-import { DB } from '../DBServer/DB'
 import { syncBitmex500msOrderBook } from './syncBitmex500msOrderBook'
-import { timeID } from '../lib/F/timeID'
 import { syncHopex500msOrderBook } from './syncHopex500msOrderBook'
+import { PKServer } from './PKServer'
 
 syncBitmex500msOrderBook()
 syncHopex500msOrderBook()
 
-const server = new JSONRPCServer({
-    funcList,
-    port: 5555
-})
-server.run()
-
-server.func.getBitmex500msOrderBook = async req => {
-    const arr = await DB.getBitmex500msOrderBook(req.symbol).findAll<{}>({
-        raw: true,
-        where: {
-            id: {
-                [Sequelize.Op.gte]: timeID.timestampTo500msID(req.startTime),
-                [Sequelize.Op.lte]: timeID.timestampTo500msID(req.endTime),
-            }
-        },
-        order: ['id'],
-    })
-    return arr.map(v => ({
-        id: v.id,
-        buy: [
-            {
-                price: v.buy1_price,
-                size: v.buy1_size,
-            },
-            {
-                price: v.buy2_price,
-                size: v.buy2_size,
-            },
-            {
-                price: v.buy3_price,
-                size: v.buy3_size,
-            },
-            {
-                price: v.buy4_price,
-                size: v.buy4_size,
-            },
-            {
-                price: v.buy5_price,
-                size: v.buy5_size,
-            },
-        ],
-        sell: [
-            {
-                price: v.sell1_price,
-                size: v.sell1_size,
-            },
-            {
-                price: v.sell2_price,
-                size: v.sell2_size,
-            },
-            {
-                price: v.sell3_price,
-                size: v.sell3_size,
-            },
-            {
-                price: v.sell4_price,
-                size: v.sell4_size,
-            },
-            {
-                price: v.sell5_price,
-                size: v.sell5_size,
-            },
-        ],
-    }))
-}
+PKServer.run()
