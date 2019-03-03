26a23566af9c48564b48758264eafb39a1ef6b38
fc01 <910677221@qq.com>
2019-03-04 00:30:02 +0800

    bt

--- src/回测.tsx
+++ src/回测.tsx
@@ -2,6 +2,8 @@ import { 回测PositionAndOrder } from './统一接口/PositionAndOrder/回测Po
 import { PositionAndOrder } from './统一接口/PositionAndOrder/PositionAndOrder'
 import { XBTUSD摸顶抄底追涨追跌 } from './统一接口/task/XBTUSD摸顶抄底追涨追跌'
 
+const start = new Date('2019-03-03T00:00:00')
+const end = new Date('2019-03-04T00:00:00')
 
-const p: PositionAndOrder = new 回测PositionAndOrder(0, 0)
+const p: PositionAndOrder = new 回测PositionAndOrder(start.getTime(), end.getTime())
 p.runTask(new XBTUSD摸顶抄底追涨追跌())
