bcea4df2516e36d51d43ba2f60ffd2d4cfac05c8
fc01 <910677221@qq.com>
2019-03-11 21:16:33 +0800

    ____

--- src/lib/C/get成交性质.ts
+++ src/lib/C/get成交性质.ts
@@ -2,8 +2,6 @@ import { BaseType } from '../BaseType'
 
 export const get成交性质 = (p: { side: BaseType.Side, 持仓量新增: number, 成交量: number }): BaseType.成交性质Type => {
 
-
-
     if (p.持仓量新增 === p.成交量) {
         return '双开'
     }
@@ -16,10 +14,20 @@ export const get成交性质 = (p: { side: BaseType.Side, 持仓量新增: numbe
     else if (p.持仓量新增 === 0 && p.side === 'Sell') {
         return '空换'
     }
-
-
-    // 这4个  咋区分 ？？
-    // '多平' | '空平' | '空开' | '多开'
-
+    else if (p.持仓量新增 < 0 && p.side === 'Buy') {
+        return '空平'
+    }
+    else if (p.持仓量新增 < 0 && p.side === 'Sell') {
+        return '多平'
+    }
+    else if (p.持仓量新增 > 0 && p.side === 'Buy') {
+        return '多开'
+    }
+    else if (p.持仓量新增 > 0 && p.side === 'Sell') {
+        return '空开'
+    }
+    else {
+        return '不知道'
+    }
 
 }
