eecae3982496592ea86543889bba7ff7bb1230e7
fc01 <910677221@qq.com>
2019-03-14 18:23:00 +0800

    ——

--- src/____备份过时____/测试.ts
+++ src/____备份过时____/测试.ts
@@ -35,39 +35,39 @@
 // tqsdk.subscribe_quote('SHFE.cu1612') 
 
 
-let request = require('request')
-let url = 'https://dataapi.joinquant.com/apis'
-let requestData = {
-    'method': 'get_token',
-    'mob': '19806580665',
-    'pwd': 'fc123456'
-}
+// let request = require('request')
+// let url = 'https://dataapi.joinquant.com/apis'
+// let requestData = {
+//     'method': 'get_token',
+//     'mob': '19806580665',
+//     'pwd': 'fc123456'
+// }
 
 
-request({
-    url: url,
-    method: 'POST',
-    body: JSON.stringify(requestData)
-}, (error, response, token) => {
+// request({
+//     url: url,
+//     method: 'POST',
+//     body: JSON.stringify(requestData)
+// }, (error, response, token) => {
 
-    console.log('token', error, response, token)
+//     console.log('token', error, response, token)
 
-    let requestData = {
-        "method": "get_ticks",
-        token,
-        "code": "000001.XSHE",
-        "count": 15,
-        "end_date": "2018-07-03"
+//     let requestData = {
+//         "method": "get_ticks",
+//         token,
+//         "code": "000001.XSHE",
+//         "count": 15,
+//         "end_date": "2018-07-03"
 
-    }
-    request({
-        url: url,
-        method: 'POST',
-        body: JSON.stringify(requestData)
-    }, function (error, response, body) {
-        console.log('get_security_info', error, response, body)
-    })
-})
+//     }
+//     request({
+//         url: url,
+//         method: 'POST',
+//         body: JSON.stringify(requestData)
+//     }, function (error, response, body) {
+//         console.log('get_security_info', error, response, body)
+//     })
+// })
 
 
 
