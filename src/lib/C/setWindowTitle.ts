116e25548f399c3715da48d1280ac020dfce0d6c
fc01 <910677221@qq.com>
2019-03-18 01:00:01 +0800

    __

--- src/lib/C/setWindowTitle.ts
+++ src/lib/C/setWindowTitle.ts
@@ -4,6 +4,6 @@ export const setWindowTitle = (title: string) => {
     try {
         remote.getCurrentWindow().setTitle(title)
     } catch (error) {
-        console.log('setTitle 出错', error)
+        // console.log('setTitle 出错', error)
     }
 }
