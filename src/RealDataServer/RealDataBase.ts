f381b73dfab9b14ef462651108bb94da713b4116
fc01 <910677221@qq.com>
2019-03-19 10:32:06 +0800

    买卖

--- src/RealDataServer/RealDataBase.ts
+++ src/RealDataServer/RealDataBase.ts
@@ -6,6 +6,7 @@ import { to范围 } from '../lib/F/to范围'
 import { lastNumber } from '../lib/F/lastNumber'
 import { is连续几根全亮 } from '../lib/F/is连续几根全亮'
 import { timeID } from '../lib/F/timeID'
+import { 买卖 } from '../指标/买卖'
 
 
 
@@ -174,34 +175,7 @@ export class RealDataBase {
     }
 
 
-    private 买卖(p: { 成交量: ArrayLike<number>, 盘口: ArrayLike<number>, 反向成交量: ArrayLike<number>, 反向盘口: ArrayLike<number> }) {
 
-        const 净成交量 = 指标.lazyMapCache(() => Math.min(p.成交量.length, p.反向成交量.length), i => p.成交量[i] - p.反向成交量[i])
-        const 净盘口 = 指标.lazyMapCache(() => Math.min(p.盘口.length, p.反向盘口.length), i => p.盘口[i] - p.反向盘口[i])
-        const 净成交量_累加5 = 指标.累加(净成交量, 5, RealDataBase.单位时间)
-        const 净成交量_累加10 = 指标.累加(净成交量, 20, RealDataBase.单位时间)
-        const 净成交量_累加7 = 指标.累加(净成交量, 7, RealDataBase.单位时间)
-        const 净成交量_累加60 = 指标.累加(净成交量, 60, RealDataBase.单位时间)
-        const 净成交量_累加500 = 指标.累加(净成交量, 500, RealDataBase.单位时间)
-        const 净成交量_累加temp = 指标.累加(净成交量, 3600, RealDataBase.单位时间)
-
-        return {
-            成交量: p.成交量,
-            盘口: p.盘口,
-            成交量_累加5: 指标.累加(p.成交量, 5, RealDataBase.单位时间),
-            //盘口_均线_1d5: 指标.均线(p.盘口, 1.5, RealDataBase.单位时间),
-            净成交量,
-            净成交量_累加5,
-            净成交量_累加7,
-            净成交量_累加10,
-            净成交量_累加60,
-            净成交量_累加500,
-            净成交量_累加temp,
-            净盘口,
-            净盘口_均线5: 指标.均线(净盘口, 3, RealDataBase.单位时间),
-            //is趋势: 指标.lazyMapCache(() => Math.min(净成交量_累加60.length), i => 净成交量_累加60[i] >= 0),//买是上涨  卖是下跌
-        }
-    }
 
 
 
@@ -242,14 +216,14 @@ export class RealDataBase {
         const __盘口买 = 指标.lazyMapCache(() => orderBook.length, i => sum(orderBook[i].buy.map(v => v.size)))
         const __盘口卖 = 指标.lazyMapCache(() => orderBook.length, i => sum(orderBook[i].sell.map(v => v.size)))
 
-        const 买 = this.买卖({
+        const 买 = 买卖({
             成交量: __成交量买,
             盘口: __盘口买,
             反向成交量: __成交量卖,
             反向盘口: __盘口卖,
         })
 
-        const 卖 = this.买卖({
+        const 卖 = 买卖({
             成交量: __成交量卖,
             盘口: __盘口卖,
             反向成交量: __成交量买,
@@ -677,7 +651,7 @@ export class RealDataBase {
                         (v__1_1 && bs.净成交量_累加5[i] < 50 * 10000) ||
                         (小 && v__1) ||
                         (大 && v__0 && v__1)
-                  
+
                     const 震荡指数_最高30 = type === '摸顶' ? 上涨.震荡指数_最高30[i] : 下跌.震荡指数_最高30[i]
                     const 震荡 = 震荡指数_最高30 > 1.1 && (震荡指数[i] < 2 && (震荡指数[i] < 1.1 || 震荡指数_macd.DIF[i] < 震荡指数_macd.DEM[i]))
 
