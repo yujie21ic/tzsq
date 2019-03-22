3de2617dce91d79b0a7e41d5f6a2906e4528b969
fc01 <910677221@qq.com>
2019-03-22 22:13:05 +0800

    HopexRESTAPI

--- src/lib/____API____/Hopex/HopexRESTAPI.ts
+++ src/lib/____API____/Hopex/HopexRESTAPI.ts
@@ -3,13 +3,11 @@ import { JSONRequest } from '../../C/JSONRequest'
 import { config } from '../../../config'
 
 
-const HopexRESTAPI__http = (p: { cookie: string, url: string, param: any }) =>
-    JSONRequest({
+const HopexRESTAPI__http = <T>(p: { cookie: string, url: string, param?: any }) =>
+    JSONRequest<T>({
         url: p.url,
-        method: 'POST',
-        body: {
-            'param': p.param
-        },
+        method: p.param ? 'POST' : 'GET',
+        body: p.param ? { param: p.param } : undefined,
         ss: config.ss,
         headers: {
             Origin: 'https://www.hopex.com',
@@ -28,7 +26,7 @@ export const HopexRESTAPI = {
         HopexRESTAPI__http({
             cookie,
             url: 'https://web.hopex.com/api/v1/gateway/User/Order?culture=zh-CN',
-            'param': {
+            param: {
                 'side': p.side === 'Sell' ? '1' : '2',
                 'orderQuantity': String(p.size),
                 'market': 'BTCUSDT',
@@ -42,7 +40,7 @@ export const HopexRESTAPI = {
         HopexRESTAPI__http({
             cookie,
             url: 'https://web.hopex.com/api/v1/gateway/User/ConditionOrder?culture=zh-CN',
-            'param': {
+            param: {
                 contractCode: 'BTCUSDT',
                 expectedPrice: String(p.side === 'Sell' ? String(p.stopPrice - 100) : String(p.stopPrice + 100)),
                 expectedQuantity: String(100000),
@@ -60,9 +58,43 @@ export const HopexRESTAPI = {
         HopexRESTAPI__http({
             cookie,
             url: 'https://web.hopex.com/api/v1/gateway/User/CancelConditionOrder?culture=zh-CN',
-            'param': {
+            param: {
                 contractCode: 'BTCUSDT',
                 taskId: p.orderID,
+            },
+        }),
+
+    getPositions: async (cookie: string) =>
+        HopexRESTAPI__http<{
+            data: {
+                positionQuantity: string // "+2"  "-2"
+            }[]
+        }>({
+            cookie,
+            url: 'https://web.hopex.com/api/v1/gateway/User/Positions?culture=zh-CN',
+        }),
+
+    getConditionOrders: async (cookie: string) =>
+        HopexRESTAPI__http<{
+            data: {
+                result: {
+                    taskId: number
+                    trigPrice: string
+                    taskTypeD: '买入止损' | '卖出止损'
+                }[]
+            }
+        }>({
+            cookie,
+            url: 'https://web.hopex.com/api/v1/gateway/User/ConditionOrders?limit=10&culture=zh-CN',
+            param: {
+                contractCode: 'BTCUSDT',
+                endTime: '0',
+                lang: 'zh-CN',
+                market: 'BTCUSDT',
+                marketCode: 'BTCUSDT',
+                side: '0',
+                startTime: '0',
+                taskStatusList: ['1'],
             },
         }),
 }
