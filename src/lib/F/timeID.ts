2175bc41a50097694b2c813d496b88d24086b34f
fc01 <910677221@qq.com>
2019-03-28 12:27:28 +0800

    __

--- src/lib/F/timeID.ts
+++ src/lib/F/timeID.ts
@@ -1,29 +1,29 @@
 export namespace timeID {
 
-    export const timestampToOneMinuteID = (time: number) =>
-        Math.floor(time / 1000 / 60)
+    export const timestampToOneMinuteID = (timestamp: number) =>
+        Math.floor(timestamp / 1000 / 60)
 
-    export const timestampTo500msID = (time: number) =>
-        Math.floor(time / 500)
-
-
-    export const oneMinuteIDToTimestamp = (index: number) =>
-        index * 1000 * 60
-
-    export const _500msIDToTimestamp = (index: number) =>
-        index * 500
+    export const timestampTo500msID = (timestamp: number) =>
+        Math.floor(timestamp / 500)
 
 
+    export const oneMinuteIDToTimestamp = (ID: number) =>
+        ID * 1000 * 60
 
-    //注释
-    export const oneMinuteIDToTimestampRange = (index: number) => ({
-        大于等于: oneMinuteIDToTimestamp(index),
-        小于: oneMinuteIDToTimestamp(index + 1),
+    export const _500msIDToTimestamp = (ID: number) =>
+        ID * 500
+
+
+
+    //ID对应的时间段
+    export const oneMinuteIDToTimestampRange = (ID: number) => ({
+        大于等于: oneMinuteIDToTimestamp(ID),
+        小于: oneMinuteIDToTimestamp(ID + 1),
     })
 
-    export const _500msIDToTimestampRange = (index: number) => ({
-        大于等于: _500msIDToTimestamp(index),
-        小于: _500msIDToTimestamp(index + 1),
+    export const _500msIDToTimestampRange = (ID: number) => ({
+        大于等于: _500msIDToTimestamp(ID),
+        小于: _500msIDToTimestamp(ID + 1),
     })
 
 }
