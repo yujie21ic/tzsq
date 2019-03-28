86ff45e4bd8aab72461ca6d145e9c6c79eb64967
fc01 <910677221@qq.com>
2019-03-28 13:44:47 +0800

    ____

--- src/lib/____API____/Hopex/HopexRESTAPI.ts
+++ src/lib/____API____/Hopex/HopexRESTAPI.ts
@@ -2,29 +2,33 @@ import { BaseType } from '../../BaseType'
 import { JSONRequest } from '../../F/JSONRequest'
 import { config } from '../../../config'
 
+const header = {
+    Origin: 'https://www.hopex.com',
+    Referer: 'https://www.hopex.com/trade?marketCode=BTCUSDT',
+    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36',
+}
 
-const HopexRESTAPI__http = <T>(p: { cookie: string, url: string, param?: any }) =>
+const HopexRESTAPI__http = <T>(p: { cookie?: string, url: string, param?: any }) =>
     JSONRequest<T>({
         url: p.url,
         method: p.param ? 'POST' : 'GET',
         body: p.param ? { param: p.param } : undefined,
         ss: config.ss,
-        headers: {
-            Origin: 'https://www.hopex.com',
-            Referer: 'https://www.hopex.com/trade?marketCode=BTCUSDT',
-            'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36',
-            conversionCurrency: 'USD',
-            Authorization: p.cookie.split('=')[1],
-            Cookie: p.cookie,
-        }
+        headers: p.cookie ?
+            {
+                ...header,
+                conversionCurrency: 'USD',
+                Authorization: p.cookie.split('=')[1],
+                Cookie: p.cookie,
+            } :
+            header
     })
 
 
 export const HopexRESTAPI = {
 
-    login: async (cookie: string, p: { userName: string, password: string }) =>
+    login: async (p: { userName: string, password: string }) =>
         HopexRESTAPI__http({
-            cookie,
             url: 'https://web.hopex.com/api/v1/User/Login?culture=zh-CN',
             param: {
                 loginType: 'pcweb',
