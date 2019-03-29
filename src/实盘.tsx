3be244950fa61b537b29e5180c00bef9dde17544
fc01 <910677221@qq.com>
2019-03-29 14:05:23 +0800

    ++

--- src/实盘.tsx
+++ src/实盘.tsx
@@ -1,21 +1,16 @@
 import * as React from 'react'
-import * as ReactDOM from 'react-dom'
-import { DataClient } from './RealDataServer/DataClient'
+import * as ReactDOM from 'react-dom' 
 import { TickBase } from './TickBase'
-import { theme } from './lib/Chart/theme'
-import { speak } from './lib/F/speak'
-import { lastNumber } from './lib/F/lastNumber'
-import { mapObjIndexed } from './lib/F/mapObjIndexed'
+import { theme } from './lib/Chart/theme' 
 import { OrderClient } from './OrderServer/OrderClient'
 import { config } from './config'
 import { BaseType } from './lib/BaseType'
 import { windowExt } from './windowExt'
 import { Button } from './lib/UI/Button'
-import { Switch } from '@material-ui/core'
-import { 指标 } from './指标/指标'
+import { Switch } from '@material-ui/core' 
 import { HopexRESTAPI } from './lib/____API____/Hopex/HopexRESTAPI'
+import { realTickClient, 提醒 } from './实盘__提醒'
 
-const realTickClient = new DataClient.RealData__Client()
 
 const account = config.account![windowExt.accountName]
 const { cookie } = account
@@ -318,79 +313,6 @@ export class 交易 extends React.Component {
 
 
 
-export class 提醒 extends React.Component {
-
-    dic: { [key: string]: number } = Object.create(null)
-
-    setAndSpeak(str: string, v: number, f: (v: number) => boolean) {
-        this.dic[str] = v
-        if (f(v)) {
-            speak(`${str}, ${v.toFixed(2)}`, str)
-        }
-    }
-
-    step = () => {
-
-        const 上轨 = lastNumber(realTickClient.dataExt.XBTUSD.bitmex._1分钟_.布林带.上轨)
-        const 下轨 = lastNumber(realTickClient.dataExt.XBTUSD.bitmex._1分钟_.布林带.下轨)
-        const 收盘价 = lastNumber(realTickClient.dataExt.XBTUSD.bitmex._1分钟_.收盘价)
-
-        this.setAndSpeak(
-            '收盘价 大于 上轨 ',
-            收盘价 - 上轨,
-            v => 收盘价 > 上轨
-        )
-
-        this.setAndSpeak(
-            '收盘价 小于 下轨 ',
-            下轨 - 收盘价,
-            v => 收盘价 < 下轨
-        )
-
-
-
-        const xxx = realTickClient.get信号灯Type('bitmex')
-        this.setAndSpeak(
-            'bitmex ' + xxx,
-            0,
-            v => xxx !== 'none'
-        )
-
-        const yyy = realTickClient.get信号灯Type('hopex')
-        this.setAndSpeak(
-            'hopex ' + yyy,
-            0,
-            v => yyy !== 'none'
-        )
-
-        const volum = realTickClient.get期货多少秒内成交量__万为单位('XBTUSD', 15)
-        const 波动率 = 指标.波动率(realTickClient.dataExt.XBTUSD.bitmex.价格, 30, 500)
-        this.setAndSpeak(
-            '比 特 币 成交量',
-            volum,
-            v => volum > 200 && lastNumber(波动率) >= 0.1
-        )
-
-
-        this.forceUpdate()
-    }
-
-    componentWillMount() {
-        const f = () => {
-            requestAnimationFrame(f)
-            this.step()
-        }
-        f()
-    }
-
-    render() {
-        return <div style={{ width: '100%', height: '100%', backgroundColor: 'black' }}>
-            <pre style={{ fontSize: 20, color: 'white' }}>
-                {JSON.stringify(mapObjIndexed(v => v.toFixed(2), this.dic), undefined, 4)}
-            </pre>
-        </div>
-    }
-}
 
 
 class Tick行情 extends TickBase {
---
+++ src/实盘__提醒.tsx
@@ -0,0 +1,82 @@
+import * as React from 'react'
+import { DataClient } from './RealDataServer/DataClient'
+import { speak } from './lib/F/speak'
+import { lastNumber } from './lib/F/lastNumber'
+import { 指标 } from './指标/指标'
+import { mapObjIndexed } from './lib/F/mapObjIndexed'
+
+export const realTickClient = new DataClient.RealData__Client()
+
+export class 提醒 extends React.Component {
+
+    dic: { [key: string]: number } = Object.create(null)
+
+    setAndSpeak(str: string, v: number, f: (v: number) => boolean) {
+        this.dic[str] = v
+        if (f(v)) {
+            speak(`${str}, ${v.toFixed(2)}`, str)
+        }
+    }
+
+    step = () => {
+
+        const 上轨 = lastNumber(realTickClient.dataExt.XBTUSD.bitmex._1分钟_.布林带.上轨)
+        const 下轨 = lastNumber(realTickClient.dataExt.XBTUSD.bitmex._1分钟_.布林带.下轨)
+        const 收盘价 = lastNumber(realTickClient.dataExt.XBTUSD.bitmex._1分钟_.收盘价)
+
+        this.setAndSpeak(
+            '收盘价 大于 上轨 ',
+            收盘价 - 上轨,
+            v => 收盘价 > 上轨
+        )
+
+        this.setAndSpeak(
+            '收盘价 小于 下轨 ',
+            下轨 - 收盘价,
+            v => 收盘价 < 下轨
+        )
+
+
+
+        const xxx = realTickClient.get信号灯Type('bitmex')
+        this.setAndSpeak(
+            'bitmex ' + xxx,
+            0,
+            v => xxx !== 'none'
+        )
+
+        const yyy = realTickClient.get信号灯Type('hopex')
+        this.setAndSpeak(
+            'hopex ' + yyy,
+            0,
+            v => yyy !== 'none'
+        )
+
+        const volum = realTickClient.get期货多少秒内成交量__万为单位('XBTUSD', 15)
+        const 波动率 = 指标.波动率(realTickClient.dataExt.XBTUSD.bitmex.价格, 30, 500)
+        this.setAndSpeak(
+            '比 特 币 成交量',
+            volum,
+            v => volum > 200 && lastNumber(波动率) >= 0.1
+        )
+
+
+        this.forceUpdate()
+    }
+
+    componentWillMount() {
+        const f = () => {
+            requestAnimationFrame(f)
+            this.step()
+        }
+        f()
+    }
+
+    render() {
+        return <div style={{ width: '100%', height: '100%', backgroundColor: 'black' }}>
+            <pre style={{ fontSize: 20, color: 'white' }}>
+                {JSON.stringify(mapObjIndexed(v => v.toFixed(2), this.dic), undefined, 4)}
+            </pre>
+        </div>
+    }
+}
