1e5bbfb5bb2d9290b103fdd9adf55e9a67315e08
fc01 <910677221@qq.com>
2019-04-04 18:27:10 +0800

    __

--- src/lib/Chart/Layer/TextLayer.ts
+++ src/lib/Chart/Layer/TextLayer.ts
@@ -30,3 +30,31 @@ export class TextLayer extends Layer<{ text: string, color: number }> {
         }
     }
 }
+
+
+export class LeftTextLayer extends Layer<{ text: string, color: number }> {
+
+    private bitmapText?: BitmapText
+
+
+    init() {
+        this.bitmapText = new BitmapText({
+            fontSize: 16,
+            fill: this.props.color,
+            anchor: {
+                x: 0,
+                y: 0
+            }
+        })
+        this.addChild(this.bitmapText)
+    }
+
+    render(viewport: Viewport, to: To) {
+        if (this.bitmapText !== undefined) {
+            this.bitmapText.text = this.props.text
+            this.bitmapText.fill = this.props.color
+            this.bitmapText.x = 10
+            this.bitmapText.y = 10
+        }
+    }
+}
