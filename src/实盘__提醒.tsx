d935e37dc5b698603fdd6179c9322101a78584aa
彭凤婷 <pengfengting@MacBook-2.local>
2019-04-06 13:25:41 +0800

    `

--- src/实盘__提醒.tsx
+++ src/实盘__提醒.tsx
@@ -3,12 +3,15 @@ import { DataClient } from './RealDataServer/DataClient'
 import { speak } from './lib/F/speak'
 import { lastNumber } from './lib/F/lastNumber'
 import { mapObjIndexed } from './lib/F/mapObjIndexed'
+import { dialog } from './lib/UI/dialog';
 
 export const realTickClient = new DataClient.RealData__Client()
 
 export class 提醒 extends React.Component {
 
     dic: { [key: string]: number } = Object.create(null)
+    小于等于 = 1000
+    大于等于 = 20000
 
     setAndSpeak(str: string, v: number, f: (v: number) => boolean) {
         this.dic[str] = v
@@ -18,12 +21,10 @@ export class 提醒 extends React.Component {
     }
 
     step = () => {
-
-
         //_______________________________布林带提醒_______________________________
         const 上轨 = lastNumber(realTickClient.dataExt.XBTUSD.bitmex._60s_.布林带.上轨)
         const 下轨 = lastNumber(realTickClient.dataExt.XBTUSD.bitmex._60s_.布林带.下轨)
-        const 收盘价 = lastNumber(realTickClient.dataExt.XBTUSD.bitmex._60s_.收盘价)
+        const 收盘价 = lastNumber(realTickClient.dataExt.XBTUSD.bitmex.收盘价)
 
         this.setAndSpeak(
             '收盘价 大于 上轨 ',
@@ -75,6 +76,38 @@ export class 提醒 extends React.Component {
 
     render() {
         return <div style={{ width: '100%', height: '100%', backgroundColor: 'black' }}>
+            <a
+                style={{ fontSize: 20, color: 'yellow' }}
+                href='#'
+                onClick={() => {
+                    dialog.showInput({
+                        title: '<=',
+                        value: this.小于等于 + '',
+                        onOK: v => {
+                            this.小于等于 = Number(v)
+                        }
+                    })
+                }}
+            >
+                {`hopex <= ${this.小于等于}`}
+            </a>
+            <br />
+            <a
+                style={{ fontSize: 20, color: 'yellow' }}
+                href='#'
+                onClick={() => {
+                    dialog.showInput({
+                        title: '>=',
+                        value: this.大于等于 + '',
+                        onOK: v => {
+                            this.大于等于 = Number(v)
+                        }
+                    })
+                }}
+            >
+                {`hopex >= ${this.大于等于}`}
+            </a>
+
             <pre style={{ fontSize: 20, color: 'white' }}>
                 {JSON.stringify(mapObjIndexed(v => v.toFixed(2), this.dic), undefined, 4)}
             </pre>
