45564ccb7f32b7aba5b5a43385ae967cb0901bd2
彭凤婷 <pengfengting@MacBook-2.local>
2019-04-06 13:28:13 +0800

    `

--- src/实盘__提醒.tsx
+++ src/实盘__提醒.tsx
@@ -1,9 +1,26 @@
 import * as React from 'react'
 import { DataClient } from './RealDataServer/DataClient'
-import { speak } from './lib/F/speak'
 import { lastNumber } from './lib/F/lastNumber'
 import { mapObjIndexed } from './lib/F/mapObjIndexed'
 import { dialog } from './lib/UI/dialog'
+
+const dic = new Map<string, number>()
+
+//同一个id一分钟内 只提醒一次
+const speak = (msg: string, id: string) => {
+
+    const n = dic.get(id)
+    if (n !== undefined && Date.now() - n < 60 * 1000) {
+        // console.log(new Date().toLocaleString(), '没有提醒', '同一个id一分钟内 只提醒一次', JSON.stringify({ body: msg }))
+        return
+    } else {
+        console.log(new Date().toLocaleString(), '提醒', JSON.stringify({ body: msg }))
+    }
+
+    dic.set(id, Date.now())
+    speechSynthesis.speak(new SpeechSynthesisUtterance(msg))
+    // new Notification('提醒', { body: msg })
+}
 
 export const realTickClient = new DataClient.RealData__Client()
 
