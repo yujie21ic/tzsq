e22cab9430ab6ac61acb049f037b2a49030111a5
fc01 <910677221@qq.com>
2019-04-13 11:15:36 +0800

    _

--- src/回测.tsx
+++ src/回测.tsx
@@ -1,28 +1,28 @@
-import { 回测PositionAndOrder } from './lib/____API____/PositionAndOrder/回测PositionAndOrder'
-import { XBTUSD摸顶抄底追涨追跌 } from './XBTUSD摸顶抄底追涨追跌'
+// import { 回测PositionAndOrder } from './lib/____API____/PositionAndOrder/回测PositionAndOrder'
+// import { XBTUSD摸顶抄底追涨追跌 } from './XBTUSD摸顶抄底追涨追跌'
 
-//  const start = new Date('2019-03-16T08:40:00')
-//  const end =   new Date('2019-03-16T08:59:00')
-const start = new Date('2019-04-01T08:10:00')
-const end =   new Date('2019-04-01T08:30:00')
+// //  const start = new Date('2019-03-16T08:40:00')
+// //  const end =   new Date('2019-03-16T08:59:00')
+// const start = new Date('2019-04-01T08:10:00')
+// const end =   new Date('2019-04-01T08:30:00')
 
-const p = new 回测PositionAndOrder(start.getTime(), end.getTime())
-// p.jsonSync.rawData.symbol.Hopex_BTC.任务开关.自动开仓抄底 = true
-// p.jsonSync.rawData.symbol.Hopex_BTC.任务开关.自动开仓摸顶 = true
-// p.jsonSync.rawData.symbol.Hopex_BTC.任务开关.自动推止损 = true
-// p.jsonSync.rawData.symbol.Hopex_BTC.任务开关.自动止盈波段 = true
- p.jsonSync.rawData.symbol.XBTUSD.任务开关.自动开仓抄底 = true
- //p.jsonSync.rawData.symbol.XBTUSD.任务开关.自动开仓摸顶 = true
-// p.jsonSync.rawData.symbol.XBTUSD.任务开关.自动开仓追涨 = false
-// p.jsonSync.rawData.symbol.XBTUSD.任务开关.自动开仓追跌 = false
-
-
-// p.jsonSync.rawData.symbol.XBTUSD.任务开关.自动开仓抄底 = false
-// p.jsonSync.rawData.symbol.XBTUSD.任务开关.自动开仓摸顶 = false
-// p.jsonSync.rawData.symbol.XBTUSD.任务开关.自动开仓追涨 = true
-// p.jsonSync.rawData.symbol.XBTUSD.任务开关.自动开仓追跌 = true
- p.jsonSync.rawData.symbol.XBTUSD.任务开关.自动推止损 = true
- p.jsonSync.rawData.symbol.XBTUSD.任务开关.自动止盈波段 = true
+// const p = new 回测PositionAndOrder(start.getTime(), end.getTime())
+// // p.jsonSync.rawData.symbol.Hopex_BTC.任务开关.自动开仓抄底 = true
+// // p.jsonSync.rawData.symbol.Hopex_BTC.任务开关.自动开仓摸顶 = true
+// // p.jsonSync.rawData.symbol.Hopex_BTC.任务开关.自动推止损 = true
+// // p.jsonSync.rawData.symbol.Hopex_BTC.任务开关.自动止盈波段 = true
+//  p.jsonSync.rawData.symbol.XBTUSD.任务开关.自动开仓抄底 = true
+//  //p.jsonSync.rawData.symbol.XBTUSD.任务开关.自动开仓摸顶 = true
+// // p.jsonSync.rawData.symbol.XBTUSD.任务开关.自动开仓追涨 = false
+// // p.jsonSync.rawData.symbol.XBTUSD.任务开关.自动开仓追跌 = false
 
 
-p.runTask(new XBTUSD摸顶抄底追涨追跌())
+// // p.jsonSync.rawData.symbol.XBTUSD.任务开关.自动开仓抄底 = false
+// // p.jsonSync.rawData.symbol.XBTUSD.任务开关.自动开仓摸顶 = false
+// // p.jsonSync.rawData.symbol.XBTUSD.任务开关.自动开仓追涨 = true
+// // p.jsonSync.rawData.symbol.XBTUSD.任务开关.自动开仓追跌 = true
+//  p.jsonSync.rawData.symbol.XBTUSD.任务开关.自动推止损 = true
+//  p.jsonSync.rawData.symbol.XBTUSD.任务开关.自动止盈波段 = true
+
+
+// p.runTask(new XBTUSD摸顶抄底追涨追跌())
