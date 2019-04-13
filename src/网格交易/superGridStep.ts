f0e2b3e35c5cf6a1040e74f6dfc668f917e6b721
fc01 <910677221@qq.com>
2019-04-13 18:16:15 +0800

    ·

--- src/网格交易/superGridStep.ts
+++ src/网格交易/superGridStep.ts
@@ -1,5 +1,71 @@
-import { range } from './node_modules/ramda'
-import { 策略运行器 } from '../WebService/策略运行器'
+//全局类型
+
+type KLineItem = {
+    index: number
+    open: number
+    close: number
+    high: number
+    low: number
+    volume: number
+}
+
+type KLineCycle = '1m' | '5m' | '1h' | '1d'
+
+type BinanceSymbol = 'btcusdt' | 'ethusdt'
+type BitmexSymbol = 'ETHUSD' | 'XBTUSD' | 'BCHZ18' | 'ADAM18' | 'BCHM18' | 'ETHM18' | 'LTCM18' | 'XBTU18' | 'XRPU18' | 'XRPZ18' | 'XBTM18'
+
+type Side = 'Buy' | 'Sell'
+
+
+
+type AAAA = {
+    网格点: (n: number) => number //n是距离   没算累计
+    格数: number
+    重挂时间: number
+    数量: (n: number) => number//n是仓位数量   算了累计
+    buy条件: (p: {
+        仓位数量: number
+        价格: number
+        开仓均价: number
+    }) => boolean
+    sell条件: (p: {
+        仓位数量: number
+        价格: number
+        开仓均价: number
+    }) => boolean
+}
+
+type SuperGridConfig = {
+    价钱除以多少: number
+    加仓: AAAA
+    减仓: AAAA
+    多仓: {
+        min: number
+        max: number
+    }
+    空仓: {
+        min: number
+        max: number
+    }
+    退出程序: (p: {
+        仓位数量: number
+        价格: number
+        开仓均价: number
+    }) => boolean
+}
+
+type RealType = {
+    config: {
+        cookie: string
+        symbol: BitmexSymbol
+    }
+    ploy: SuperGridConfig
+}
+
+declare const real: RealType
+
+
+
 import { getGridPrice } from './getGridPrice'
 import * as electron from 'electron'
 
