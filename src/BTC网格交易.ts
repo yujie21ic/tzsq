95e54aa6dd194892994925fc6345407ba955a76f
fc01 <910677221@qq.com>
2019-04-14 20:43:42 +0800

    `

--- src/BTC网格交易.ts
+++ src/BTC网格交易.ts
@@ -23,7 +23,6 @@ type AAAA = {
 }
 
 type 参数 = {
-    价钱除以多少: number
     加仓: AAAA
     减仓: AAAA
     多仓: {
@@ -41,256 +40,146 @@ let 网格配置: 参数
 let api: any
 
 
-const get加仓数量 = (累计: number) => 网格配置.加仓.get单个格子数量(累计 + Math.abs(api.myPosition))
-
-const get减仓数量 = (累计: number) => 网格配置.减仓.get单个格子数量(累计 + Math.abs(api.myPosition))
-
-const get开仓均价 = () => Math.round(api.开仓均价 * 网格配置.价钱除以多少)
-
-const getLastFillPrice = () => Math.round(api.lastFillPrice * 网格配置.价钱除以多少)
-
-const getBuy1 = () => Math.round(api.buy1 * 网格配置.价钱除以多少)
-
-const getSell1 = () => Math.round(api.sell1 * 网格配置.价钱除以多少)
-
-
-const __getPrice = (side: BaseType.Side, 网格点: number, _price_?: number, 必须盈利 = true) => {
-    // console.log(side, '必须盈利', 必须盈利)
-    if (side === 'Buy') {
-        const arr = [_price_, getSell1(), ...[必须盈利 ? [get开仓均价()] : []]].filter(v => v !== 0 && v !== undefined) as number[]
-        let price = Math.min(...arr)
-
-        //多仓.最大价格 删除
-
-        return to价格对齐({
-            side: 'Buy',
-            value: price,
-            grid: 网格点
-        })
-    } else {
-        const arr = [_price_, getBuy1(), ...[必须盈利 ? [get开仓均价()] : []]].filter(v => v !== 0 && v !== undefined) as number[]
-        let price = Math.max(...arr)
-
-        //空仓.最小价格 删除
-
-        return to价格对齐({
-            side: 'Sell',
-            value: price,
-            grid: 网格点
-        })
-    }
-}
-
-const getPrice = (side: BaseType.Side, 网格点: number, 重挂时间: number, 必须盈利: boolean) => {
-    let p = __getPrice(side, 网格点, undefined, 必须盈利)
-
-    //重挂时间内
-    if (api.lastFillSide === side &&
-        (
-            (side === 'Buy' && p >= getLastFillPrice()) ||
-            (side === 'Sell' && p <= getLastFillPrice())
-        ) &&
-        Date.now() < api.lastFillTime + 重挂时间 * 1000
-    ) {
-        p = __getPrice(side, 网格点, getLastFillPrice(), 必须盈利)
-    }
-
-    return p
-}
-
-const getOrderArr = (side: BaseType.Side, 网格点: number, 格数: number, 重挂时间: number) => {
-    let p = getPrice(side, 网格点, 重挂时间, true)
-
-    //多仓小于min 亏损加仓
-    if (side === 'Buy' && api.myPosition > 0 && api.myPosition < 网格配置.多仓.min) {
-        p = getPrice('Buy', 网格点, 重挂时间, false)
-    }
-
-    //空仓小于min 亏损加仓 
-    if (side === 'Sell' && api.myPosition < 0 && Math.abs(api.myPosition) < 网格配置.空仓.min) {
-        p = getPrice('Sell', 网格点, 重挂时间, false)
-    }
-
-    let 累计 = 0
-    return range(0, 格数).map(i => {
-        const price = side === 'Buy' ? p - i * 网格点 : p + i * 网格点
-        let count: number
-        if (side === 'Buy') {
-            count = api.myPosition > 0 ? get加仓数量(累计) : get减仓数量(累计)
-        } else {
-            count = api.myPosition < 0 ? get加仓数量(累计) : get减仓数量(累计)
-        }
-        累计 += count
-        return { side, price, count }
-    })
-}
-
-const step = () => {
-
-
-    const 减仓距离 = get开仓均价() === 0 ? 0 : Math.abs(get开仓均价() - (api.myPosition > 0 ? getSell1() : getBuy1()))
-    const 减仓 = getOrderArr(
-        api.myPosition > 0 ? 'Sell' : 'Buy',
-        网格配置.减仓.get单个格子大小(减仓距离),
-        网格配置.减仓.格数,
-        网格配置.减仓.重挂时间
-    ).filter(v => v.side === 'Buy' ? 网格配置.减仓.buy条件({
-        仓位数量: api.myPosition,
-        价格: v.price,
-        开仓均价: get开仓均价()
-    }) : 网格配置.减仓.sell条件({
-        仓位数量: api.myPosition,
-        价格: v.price,
-        开仓均价: get开仓均价()
-    }))
-
-    const 加仓距离 = get开仓均价() === 0 ? 0 : Math.abs(get开仓均价() - (api.myPosition > 0 ? getBuy1() : getSell1()))
-    const 加仓 = getOrderArr(
-        api.myPosition > 0 ? 'Buy' : 'Sell',
-        网格配置.加仓.get单个格子大小(加仓距离),
-        网格配置.加仓.格数,
-        网格配置.加仓.重挂时间
-    ).filter(v => v.side === 'Buy' ? 网格配置.加仓.buy条件({
-        仓位数量: api.myPosition,
-        价格: v.price,
-        开仓均价: get开仓均价()
-    }) : 网格配置.加仓.sell条件({
-        仓位数量: api.myPosition,
-        价格: v.price,
-        开仓均价: get开仓均价()
-    }))
-
-    let arr: { side: BaseType.Side, price: number, count: number }[] = []
-
-    let count = 0
-    let temp = false
-    for (let i = 0; i < 减仓.length; i++) {
-        count += 减仓[i].count
-        //没有仓位 Buy
-        if (api.myPosition === 0) {
-            if (count > 网格配置.多仓.max) break
-        }
-        //多仓
-        if (api.myPosition > 0) {
-            //不能减仓
-            if (网格配置.多仓.min !== 0 && api.myPosition < 网格配置.多仓.min) {
-                break
-            }
-
-            //减仓后不能 反手 > 最大仓位
-            if (api.myPosition - count < -网格配置.空仓.max) {
-                break
-            }
-
-            //减仓后 < 最小仓位 下一格就不减仓了
-            if (网格配置.多仓.min !== 0 && api.myPosition - count < 网格配置.多仓.min) {
-                temp = true
-            }
-        }
-        //空仓
-        if (api.myPosition < 0) {
-            //不能减仓
-            if (网格配置.空仓.min !== 0 && api.myPosition > -网格配置.空仓.min) {
-                break
-            }
-
-            //减仓后不能 反手 > 最大仓位
-            if (api.myPosition + count > 网格配置.多仓.max) {
-                break
-            }
-
-            //减仓后 < 最小仓位 下一格就不减仓了
-            if (网格配置.空仓.min !== 0 && api.myPosition + count > -网格配置.空仓.min) {
-                temp = true
-            }
-        }
-        arr.push(减仓[i])
-        if (temp) break
-    }
-
-
-    count = 0
-    for (let i = 0; i < 加仓.length; i++) {
-        count += 加仓[i].count
-        //没有仓位 Sell
-        if (api.myPosition === 0) {
-            if (count > 网格配置.空仓.max) break
-        }
-        //多仓
-        if (api.myPosition > 0) {
-            //加仓后不能 > 最大仓位
-            if (api.myPosition + count > 网格配置.多仓.max) {
-                break
-            }
-        }
-        //空仓
-        if (api.myPosition < 0) {
-            //加仓后不能 > 最大仓位
-            if (api.myPosition - count < -网格配置.空仓.max) {
-                break
-            }
-        }
-        arr.push(加仓[i])
-    }
-    // console.log(JSON.stringify(arr))
-    sync活动委托(arr)
-}
-
-//price 不能重复
-const sync活动委托 = (arr: { side: BaseType.Side, price: number, count: number }[]) => {
-
-    let dic: { [price: number]: { side: BaseType.Side, count: number } } = {}
-
-    arr.forEach(v => {
-        dic[v.price] = { side: v.side, count: v.count }
-    })
-
-
-    let cancelIDs: string[] = []
-
-    api.活动委托.forEach(v => {
-
-
-        const PRICE = Math.round(v.price * 网格配置.价钱除以多少)//<--------- 
-
-        //这个价格没有委托 取消掉
-        if (dic[PRICE] === undefined) {
-            cancelIDs.push(v.orderID)
-        }
-        // 委托数量不一样 取消掉
-        else if (v.orderQty !== dic[PRICE].count) {
-            cancelIDs.push(v.orderID)
-        }
-        //委托数量一样
-        else {
-            delete dic[PRICE]
-        }
-    })
-
-
-    if (cancelIDs.length !== 0) {
-        api.orderCancel(cancelIDs)
-    } else {
-        let arr: { side: BaseType.Side, price: number, count: number }[] = []
-        for (const price in dic) {
-            arr.push({
-                side: dic[price].side,
-                price: Number(price) / 网格配置.价钱除以多少,//<---------
-                count: dic[price].count,
-            })
-        }
-        api.order(arr)
-    }
-}
-
-
-
-
 
 import { PositionAndOrder } from './lib/____API____/PositionAndOrder/PositionAndOrder'
 import { PositionAndOrderTask } from './lib/____API____/PositionAndOrder/PositionAndOrder'
 
 export class BTC网格交易 implements PositionAndOrderTask {
+
+
+    get加仓数量 = (累计: number) => 网格配置.加仓.get单个格子数量(累计 + Math.abs(api.myPosition))
+
+    get减仓数量 = (累计: number) => 网格配置.减仓.get单个格子数量(累计 + Math.abs(api.myPosition))
+
+    get开仓均价 = () => api.开仓均价
+
+    getLastFillPrice = () => api.lastFillPrice
+
+    getBuy1 = () => api.buy1
+
+    getSell1 = () => api.sell1
+
+
+    __getPrice = (side: BaseType.Side, 网格点: number, _price_?: number, 必须盈利 = true) => {
+        // console.log(side, '必须盈利', 必须盈利)
+        if (side === 'Buy') {
+            const arr = [_price_, this.getSell1(), ...[必须盈利 ? [this.get开仓均价()] : []]].filter(v => v !== 0 && v !== undefined) as number[]
+            let price = Math.min(...arr)
+
+            //多仓.最大价格 删除
+
+            return to价格对齐({
+                side: 'Buy',
+                value: price,
+                grid: 网格点
+            })
+        } else {
+            const arr = [_price_, this.getBuy1(), ...[必须盈利 ? [this.get开仓均价()] : []]].filter(v => v !== 0 && v !== undefined) as number[]
+            let price = Math.max(...arr)
+
+            //空仓.最小价格 删除
+
+            return to价格对齐({
+                side: 'Sell',
+                value: price,
+                grid: 网格点
+            })
+        }
+    }
+
+    getPrice = (side: BaseType.Side, 网格点: number, 重挂时间: number, 必须盈利: boolean) => {
+        let p = this.__getPrice(side, 网格点, undefined, 必须盈利)
+
+        //重挂时间内
+        if (api.lastFillSide === side &&
+            (
+                (side === 'Buy' && p >= this.getLastFillPrice()) ||
+                (side === 'Sell' && p <= this.getLastFillPrice())
+            ) &&
+            Date.now() < api.lastFillTime + 重挂时间 * 1000
+        ) {
+            p = this.__getPrice(side, 网格点, this.getLastFillPrice(), 必须盈利)
+        }
+
+        return p
+    }
+
+    getOrderArr = (side: BaseType.Side, 网格点: number, 格数: number, 重挂时间: number) => {
+        let p = this.getPrice(side, 网格点, 重挂时间, true)
+
+        //多仓小于min 亏损加仓
+        if (side === 'Buy' && api.myPosition > 0 && api.myPosition < 网格配置.多仓.min) {
+            p = this.getPrice('Buy', 网格点, 重挂时间, false)
+        }
+
+        //空仓小于min 亏损加仓 
+        if (side === 'Sell' && api.myPosition < 0 && Math.abs(api.myPosition) < 网格配置.空仓.min) {
+            p = this.getPrice('Sell', 网格点, 重挂时间, false)
+        }
+
+        let 累计 = 0
+        return range(0, 格数).map(i => {
+            const price = side === 'Buy' ? p - i * 网格点 : p + i * 网格点
+            let count: number
+            if (side === 'Buy') {
+                count = api.myPosition > 0 ? this.get加仓数量(累计) : this.get减仓数量(累计)
+            } else {
+                count = api.myPosition < 0 ? this.get加仓数量(累计) : this.get减仓数量(累计)
+            }
+            累计 += count
+            return { side, price, count }
+        })
+    }
+
+
+    //price 不能重复
+    sync活动委托 = (arr: { side: BaseType.Side, price: number, count: number }[]) => {
+
+        let dic: { [price: number]: { side: BaseType.Side, count: number } } = {}
+
+        arr.forEach(v => {
+            dic[v.price] = { side: v.side, count: v.count }
+        })
+
+
+        let cancelIDs: string[] = []
+
+        api.活动委托.forEach(v => {
+
+
+            const PRICE = v.price
+
+            //这个价格没有委托 取消掉
+            if (dic[PRICE] === undefined) {
+                cancelIDs.push(v.orderID)
+            }
+            // 委托数量不一样 取消掉
+            else if (v.orderQty !== dic[PRICE].count) {
+                cancelIDs.push(v.orderID)
+            }
+            //委托数量一样
+            else {
+                delete dic[PRICE]
+            }
+        })
+
+
+        if (cancelIDs.length !== 0) {
+            api.orderCancel(cancelIDs)
+        } else {
+            let arr: { side: BaseType.Side, price: number, count: number }[] = []
+            for (const price in dic) {
+                arr.push({
+                    side: dic[price].side,
+                    price: Number(price),
+                    count: dic[price].count,
+                })
+            }
+            api.order(arr)
+        }
+    }
+
+
 
     onFilled(p: { symbol: BaseType.BitmexSymbol, type: '限价' | '限价只减仓' | '止损' | '强平' }) {
 
@@ -300,7 +189,116 @@ export class BTC网格交易 implements PositionAndOrderTask {
         return false
     }
 
+
     onTick(self: PositionAndOrder) {
+        const 减仓距离 = this.get开仓均价() === 0 ? 0 : Math.abs(this.get开仓均价() - (api.myPosition > 0 ? this.getSell1() : this.getBuy1()))
+        const 减仓 = this.getOrderArr(
+            api.myPosition > 0 ? 'Sell' : 'Buy',
+            网格配置.减仓.get单个格子大小(减仓距离),
+            网格配置.减仓.格数,
+            网格配置.减仓.重挂时间
+        ).filter(v => v.side === 'Buy' ? 网格配置.减仓.buy条件({
+            仓位数量: api.myPosition,
+            价格: v.price,
+            开仓均价: this.get开仓均价()
+        }) : 网格配置.减仓.sell条件({
+            仓位数量: api.myPosition,
+            价格: v.price,
+            开仓均价: this.get开仓均价()
+        }))
+
+        const 加仓距离 = this.get开仓均价() === 0 ? 0 : Math.abs(this.get开仓均价() - (api.myPosition > 0 ? this.getBuy1() : this.getSell1()))
+        const 加仓 = this.getOrderArr(
+            api.myPosition > 0 ? 'Buy' : 'Sell',
+            网格配置.加仓.get单个格子大小(加仓距离),
+            网格配置.加仓.格数,
+            网格配置.加仓.重挂时间
+        ).filter(v => v.side === 'Buy' ? 网格配置.加仓.buy条件({
+            仓位数量: api.myPosition,
+            价格: v.price,
+            开仓均价: this.get开仓均价()
+        }) : 网格配置.加仓.sell条件({
+            仓位数量: api.myPosition,
+            价格: v.price,
+            开仓均价: this.get开仓均价()
+        }))
+
+        let arr: { side: BaseType.Side, price: number, count: number }[] = []
+
+        let count = 0
+        let temp = false
+        for (let i = 0; i < 减仓.length; i++) {
+            count += 减仓[i].count
+            //没有仓位 Buy
+            if (api.myPosition === 0) {
+                if (count > 网格配置.多仓.max) break
+            }
+            //多仓
+            if (api.myPosition > 0) {
+                //不能减仓
+                if (网格配置.多仓.min !== 0 && api.myPosition < 网格配置.多仓.min) {
+                    break
+                }
+
+                //减仓后不能 反手 > 最大仓位
+                if (api.myPosition - count < -网格配置.空仓.max) {
+                    break
+                }
+
+                //减仓后 < 最小仓位 下一格就不减仓了
+                if (网格配置.多仓.min !== 0 && api.myPosition - count < 网格配置.多仓.min) {
+                    temp = true
+                }
+            }
+            //空仓
+            if (api.myPosition < 0) {
+                //不能减仓
+                if (网格配置.空仓.min !== 0 && api.myPosition > -网格配置.空仓.min) {
+                    break
+                }
+
+                //减仓后不能 反手 > 最大仓位
+                if (api.myPosition + count > 网格配置.多仓.max) {
+                    break
+                }
+
+                //减仓后 < 最小仓位 下一格就不减仓了
+                if (网格配置.空仓.min !== 0 && api.myPosition + count > -网格配置.空仓.min) {
+                    temp = true
+                }
+            }
+            arr.push(减仓[i])
+            if (temp) break
+        }
+
+
+        count = 0
+        for (let i = 0; i < 加仓.length; i++) {
+            count += 加仓[i].count
+            //没有仓位 Sell
+            if (api.myPosition === 0) {
+                if (count > 网格配置.空仓.max) break
+            }
+            //多仓
+            if (api.myPosition > 0) {
+                //加仓后不能 > 最大仓位
+                if (api.myPosition + count > 网格配置.多仓.max) {
+                    break
+                }
+            }
+            //空仓
+            if (api.myPosition < 0) {
+                //加仓后不能 > 最大仓位
+                if (api.myPosition - count < -网格配置.空仓.max) {
+                    break
+                }
+            }
+            arr.push(加仓[i])
+        }
+        // console.log(JSON.stringify(arr))
+        this.sync活动委托(arr)
         return true
     }
+
+
 }
