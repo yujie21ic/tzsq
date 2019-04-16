0285214bc08c39d1d46f8640b94a73554a7f3a52
fc01 <910677221@qq.com>
2019-04-16 15:59:43 +0800

    ·

--- src/task/BTC网格交易.ts
+++ src/task/BTC网格交易.ts
@@ -3,14 +3,31 @@ import { range } from 'ramda'
 import { PositionAndOrder } from '../lib/____API____/PositionAndOrder/PositionAndOrder'
 import { PositionAndOrderTask } from '../lib/____API____/PositionAndOrder/PositionAndOrder'
 import { lastNumber } from '../lib/F/lastNumber'
-import { BTC网格交易__参数 } from './BTC网格交易__参数'
 import { to价格对齐 } from '../lib/F/to价格对齐'
 
 export class BTC网格交易 implements PositionAndOrderTask {
 
     开关 = false
-    参数type = {}
-    参数 = {}
+    参数type = {
+        单个格子大小: 2.5,
+        单个格子数量: 25,
+        格数: 5,
+        方向: 'Sell' as BaseType.Side,
+        最大仓位: 1000,
+        盈利加仓: true,
+        加仓: true,
+        减仓: true,
+    }
+    参数 = {
+        单个格子大小: 2.5,
+        单个格子数量: 25,
+        格数: 5,
+        方向: 'Sell' as BaseType.Side,
+        最大仓位: 1000,
+        盈利加仓: true,
+        加仓: true,
+        减仓: true,
+    }
 
     private self = {} as PositionAndOrder
     private lastBuyPrice = NaN
@@ -28,8 +45,8 @@ export class BTC网格交易 implements PositionAndOrderTask {
         range(0, 50).map(i =>
             ({
                 side,
-                price: side === 'Buy' ? price - i * BTC网格交易__参数.单个格子大小 : price + i * BTC网格交易__参数.单个格子大小,
-                size: BTC网格交易__参数.单个格子数量,
+                price: side === 'Buy' ? price - i * this.参数.单个格子大小 : price + i * this.参数.单个格子大小,
+                size: this.参数.单个格子数量,
                 reduceOnly,
             })
         )
@@ -67,30 +84,30 @@ export class BTC网格交易 implements PositionAndOrderTask {
     }
 
     private get减仓() {
-        if (BTC网格交易__参数.减仓 === false) return []
+        if (this.参数.减仓 === false) return []
 
         const count = this.get仓位数量()
-        const 最大格数 = Math.floor(Math.abs(count) / BTC网格交易__参数.单个格子大小)
-        const 格数 = Math.min(最大格数, BTC网格交易__参数.格数)
+        const 最大格数 = Math.floor(Math.abs(count) / this.参数.单个格子大小)
+        const 格数 = Math.min(最大格数, this.参数.格数)
 
-        if (BTC网格交易__参数.方向 === 'Buy' && count > 0) {
+        if (this.参数.方向 === 'Buy' && count > 0) {
             return this.toList({
                 side: 'Sell',
                 price: to价格对齐({
                     side: 'Sell',
                     value: Math.max(this.getSell1(), this.get开仓均价()),
-                    grid: BTC网格交易__参数.单个格子大小,
+                    grid: this.参数.单个格子大小,
                 }),
                 reduceOnly: true,
             }).filter(this.同一个价位不连续挂2次).slice(0, 格数)
         }
-        else if (BTC网格交易__参数.方向 === 'Sell' && count < 0) {
+        else if (this.参数.方向 === 'Sell' && count < 0) {
             return this.toList({
                 side: 'Buy',
                 price: to价格对齐({
                     side: 'Buy',
                     value: Math.min(this.getBuy1(), this.get开仓均价()),
-                    grid: BTC网格交易__参数.单个格子大小,
+                    grid: this.参数.单个格子大小,
                 }),
                 reduceOnly: true,
             }).filter(this.同一个价位不连续挂2次).slice(0, 格数)
@@ -101,27 +118,27 @@ export class BTC网格交易 implements PositionAndOrderTask {
 
 
     private get加仓() {
-        if (BTC网格交易__参数.加仓 === false) return []
+        if (this.参数.加仓 === false) return []
 
         const count = this.get仓位数量()
-        const 最大格数 = Math.floor((BTC网格交易__参数.最大仓位 - Math.abs(count)) / BTC网格交易__参数.单个格子大小)
-        let 格数 = Math.min(最大格数, BTC网格交易__参数.格数)
+        const 最大格数 = Math.floor((this.参数.最大仓位 - Math.abs(count)) / this.参数.单个格子大小)
+        let 格数 = Math.min(最大格数, this.参数.格数)
 
         if (
-            (BTC网格交易__参数.方向 === 'Sell' && count > 0) ||
-            (BTC网格交易__参数.方向 === 'Buy' && count < 0)
+            (this.参数.方向 === 'Sell' && count > 0) ||
+            (this.参数.方向 === 'Buy' && count < 0)
         ) {
-            格数 = BTC网格交易__参数.格数
+            格数 = this.参数.格数
         }
 
         return this.toList({
-            side: BTC网格交易__参数.方向,
+            side: this.参数.方向,
             price: to价格对齐({
-                side: BTC网格交易__参数.方向,
-                value: BTC网格交易__参数.方向 === 'Sell' ?
-                    (BTC网格交易__参数.盈利加仓 ? this.getSell1() : Math.max(this.getSell1(), this.get开仓均价())) :
-                    (BTC网格交易__参数.盈利加仓 ? this.getBuy1() : Math.min(this.getBuy1(), this.get开仓均价())),
-                grid: BTC网格交易__参数.单个格子大小,
+                side: this.参数.方向,
+                value: this.参数.方向 === 'Sell' ?
+                    (this.参数.盈利加仓 ? this.getSell1() : Math.max(this.getSell1(), this.get开仓均价())) :
+                    (this.参数.盈利加仓 ? this.getBuy1() : Math.min(this.getBuy1(), this.get开仓均价())),
+                grid: this.参数.单个格子大小,
             }),
             reduceOnly: false,
         }).filter(this.同一个价位不连续挂2次).slice(0, 格数)
