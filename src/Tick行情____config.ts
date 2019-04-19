5b51379e37e9f7e836c2be813ec79247ead49b0a
fc01 <910677221@qq.com>
2019-04-19 21:56:34 +0800

    ·

--- src/Tick行情____config.ts
+++ src/Tick行情____config.ts
@@ -73,12 +73,12 @@ export const Tick行情____config: { [key in string]: ItemFunc } = {
                 ]
             },],
             {
-                layerList: [                    
+                layerList: [
                     layer(LineLayer, { data: d2.XBTUSD.bitmex.盘口卖1加被吃的, color: 幽灵卖颜色 }),
-                    layer(LineLayer, { data: d2.XBTUSD.bitmex.买.盘口1, color: 卖颜色1  }),
+                    layer(LineLayer, { data: d2.XBTUSD.bitmex.卖.盘口1, color: 卖颜色1 }),
                     layer(ZeroLayer, { color: 0xaaaaaa }),
-                    layer(LineLayer, { data: d2.XBTUSD.bitmex.盘口买1加被吃的, color: 幽灵买颜色 , 临时参数: '变成负数'}),
-                    layer(LineLayer, { data: d2.XBTUSD.bitmex.卖.盘口1, color: 买颜色1 , 临时参数: '变成负数'}),                    
+                    layer(LineLayer, { data: d2.XBTUSD.bitmex.盘口买1加被吃的, color: 幽灵买颜色, 临时参数: '变成负数' }),
+                    layer(LineLayer, { data: d2.XBTUSD.bitmex.买.盘口1, color: 买颜色1, 临时参数: '变成负数' }),
                 ]
             }
         ]
