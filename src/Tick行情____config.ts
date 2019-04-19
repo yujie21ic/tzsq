7053db2c8ea370ad7d4c175e761815f97e9af6b4
schu <workfor2012@gmail.com>
2019-04-19 23:58:34 +0800

    更新图表

--- src/Tick行情____config.ts
+++ src/Tick行情____config.ts
@@ -2896,15 +2896,65 @@ export const Tick行情____config: { [key in string]: ItemFunc } = {
 
         ]
     }),
-    '螺纹成交量1': (d, d2) => ({
+    '螺纹成交量2': (d, d2) => ({
         heightList: [1],
         items: [
             [
                 {
                     layerList: [
                         layer(ZeroLayer, { color: 0xaaaaaa }),
-                        layer(LineLayer, { data: d2.ctp.rb_zhuli.买.买成交量累加, color: 买颜色 }),
-                        layer(LineLayer, { data: d2.ctp.rb_zhuli.买.卖成交量累加, color: 卖颜色 }),
+                        layer(LineLayer, { data: d2.ctp.rb_zhuli.价格乘以ln净成交量, color: 买颜色 }),
+                        //layer(LineLayer, { data: d2.ctp.rb_zhuli.盘口买1加被吃的, color: 幽灵买颜色, 临时参数: '变成负数' }),
+                        //layer(LineLayer, { data: d2.ctp.rb_zhuli.买.盘口1, color: 买颜色1, 临时参数: '变成负数' }),
+                        //layer(LineLayer, { data: d2.ctp.rb_zhuli.买.净盘口_均线3, color: ETH颜色 }),
+                        //layer(LineLayer, { data: d2.ctp.rb_zhuli.盘口卖1加被吃的, color: 幽灵卖颜色 }),
+                        //layer(LineLayer, { data: d2.ctp.rb_zhuli.卖.盘口1, color: 卖颜色1 }),
+                    ]
+                },
+                // {
+                //     numberColor: 波动率颜色,
+                //     layerList: [
+                //         layer(ZeroLayer, { color: 波动率颜色 }),
+                //         layer(LineLayer, { data: d2.ctp.rb_zhuli.买.净成交量_累加60, color: 波动率颜色 }),
+                //     ]
+                // },
+                {
+                    numberColor: BTC颜色,
+                    layerList: [
+                        layer(LineLayer, { data: d2.ctp.rb_zhuli.价格, color: BTC颜色 }),
+                    ]
+                },
+
+            ],
+            // {
+            //     layerList: [
+            //         layer(ZeroLayer, { color: 0xaaaaaa }),
+            //         layer(LineLayer, { data: d2.ctp.rb_zhuli.买.成交量, color: 买颜色 }),
+            //         layer(LineLayer, { data: d2.ctp.rb_zhuli.卖.成交量, color: 卖颜色, 临时参数: '变成负数' }),
+            //         layer(TextLayer, { text: '成交量      ', color: 0xffff00 })
+            //     ]
+            // },
+            // {
+            //     layerList: [
+            //         layer(ZeroLayer, { color: 0xaaaaaa }),
+            //         layer(LineLayer, { data: d2.ctp.rb_zhuli.买.盘口, color: 买颜色 }),
+            //         layer(LineLayer, { data: d2.ctp.rb_zhuli.卖.盘口, color: 卖颜色, 临时参数: '变成负数' }),
+            //         layer(LineLayer, { data: d2.ctp.rb_zhuli.买.净盘口_均线3, color: ETH颜色 }),
+            //         layer(TextLayer, { text: '盘口买 盘口卖      ', color: 0xffff00 })
+            //     ]
+            // },
+
+        ]
+    }),
+    '螺纹成交量3': (d, d2) => ({
+        heightList: [1],
+        items: [
+            [
+                {
+                    layerList: [
+                        layer(ZeroLayer, { color: 0xaaaaaa }),
+                        layer(LineLayer, { data: d2.ctp.rb_zhuli.价格乘以ln主动买, color: 买颜色 }),
+                        layer(LineLayer, { data: d2.ctp.rb_zhuli.价格乘以ln主动卖, color: 卖颜色 }),
                         //layer(LineLayer, { data: d2.ctp.rb_zhuli.盘口买1加被吃的, color: 幽灵买颜色, 临时参数: '变成负数' }),
                         //layer(LineLayer, { data: d2.ctp.rb_zhuli.买.盘口1, color: 买颜色1, 临时参数: '变成负数' }),
                         //layer(LineLayer, { data: d2.ctp.rb_zhuli.买.净盘口_均线3, color: ETH颜色 }),
