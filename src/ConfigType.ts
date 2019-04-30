3b4e76870733265be4fa4f084f78e7b6feae4d91
fc01 <910677221@qq.com>
2019-04-30 14:40:39 +0800

    （config.下单数量 || 10000) * this.props.倍数

--- src/ConfigType.ts
+++ src/ConfigType.ts
@@ -2,6 +2,7 @@ export type ConfigType = {
     ss: boolean
     orderServerIP?: string
     tick历史加载多少秒?: number
+    下单数量?:number
     orderServer?: {
         [name: string]: {
             cookie: string
