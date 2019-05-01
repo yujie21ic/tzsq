a1ceba02adda4d944925418dcac1cd14d72be6dd
fc01 <910677221@qq.com>
2019-05-01 15:05:27 +0800

    ··

--- src/task/BTC止损.ts
+++ src/task/BTC止损.ts
@@ -31,24 +31,30 @@ export class BTC止损 implements PositionAndOrderTask {
 
         const 止损点 = (this.参数.A ? 5 : 0) + (this.参数.B ? 5 : 0) + (this.参数.C ? 5 : 0)
 
-        if (止损点 === 0) return false
-
         const { 仓位数量, 开仓均价, 委托列表 } = self.jsonSync.rawData.market.bitmex.XBTUSD
 
         const 止损委托 = 委托列表.filter(v => v.type === '止损')
 
+
+        if (止损点 === 0) {
+            if (止损委托.length !== 0) {
+                return self.cancel({ orderID: 止损委托.map(v => v.id) })
+            } else {
+                return false
+            }
+        }
+
+        const side = 仓位数量 > 0 ? 'Sell' : 'Buy'
+        const 止损价格 = to价格对齐({
+            grid: 0.5,
+            value: 仓位数量 > 0 ? 开仓均价 - 止损点 : 开仓均价 + 止损点,
+            side,
+        })
+
         //没有止损 
         if (止损委托.length === 0) {
             if (仓位数量 !== 0) {
-                const side = 仓位数量 > 0 ? 'Sell' : 'Buy'
-                return self.stop({
-                    side,
-                    price: to价格对齐({
-                        grid: 0.5,
-                        value: 仓位数量 > 0 ? 开仓均价 - 止损点 : 开仓均价 + 止损点,
-                        side,
-                    }),
-                })
+                return self.stop({ side, price: 止损价格, })
             }
             else {
                 return false
@@ -56,21 +62,19 @@ export class BTC止损 implements PositionAndOrderTask {
         }
         //有止损
         else if (止损委托.length === 1) {
-            //没有仓位 或者 止损方向错了
+            //止损方向错了
             if (仓位数量 === 0 || (仓位数量 > 0 && 止损委托[0].side !== 'Sell') || (仓位数量 < 0 && 止损委托[0].side !== 'Buy')) {
                 return self.cancel({ orderID: 止损委托.map(v => v.id) })
+            }
+            //止损价格错了
+            else if (止损委托[0].price !== 止损价格) {
+                return self.updateStop({ orderID: 止损委托[0].id, price: 止损价格 })
             }
             else {
                 return false
             }
         }
-        else if (止损委托.length === 2 && 止损委托[0].side === 止损委托[1].side) {
-            if (止损委托[0].side === 'Sell') {
-                return self.cancel({ orderID: [止损委托[0].price < 止损委托[1].price ? 止损委托[0].id : 止损委托[1].id] })
-            } else {
-                return self.cancel({ orderID: [止损委托[0].price > 止损委托[1].price ? 止损委托[0].id : 止损委托[1].id] })
-            }
-        }
+        //多个止损 全取消
         else {
             return self.cancel({ orderID: 止损委托.map(v => v.id) })
         }
