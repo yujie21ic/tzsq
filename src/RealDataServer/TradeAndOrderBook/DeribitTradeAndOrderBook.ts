335f502d49b1ca03630a33a5e8670abfe91e79f5
fc01 <910677221@qq.com>
2019-05-20 19:41:31 +0800

    ```

--- src/RealDataServer/TradeAndOrderBook/DeribitTradeAndOrderBook.ts
+++ src/RealDataServer/TradeAndOrderBook/DeribitTradeAndOrderBook.ts
@@ -1,11 +1,37 @@
 import { TradeAndOrderBook } from './TradeAndOrderBook'
 import { BaseType } from '../../lib/BaseType'
+import { WebSocketClient } from '../../lib/F/WebSocketClient'
+import { config } from '../../config'
 
 export class DeribitTradeAndOrderBook extends TradeAndOrderBook<BaseType.BitmexSymbol> {
 
     name = 'deribit'
 
+    private ws = new WebSocketClient({
+        name: 'deribit ws',
+        ss: config.ss,
+        url: 'wss://www.deribit.com/ws/api/v1/',
+    })
+
     constructor() {
         super()
+
+        this.ws.onStatusChange = () => {
+            if (this.ws.isConnected) {
+                this.ws.sendJSON({
+                    'id': 1234,
+                    'action': '/api/v1/public/getorderbook',
+                    'arguments': {
+                        'instrument': ['futures'],
+                        'event': ['order_book', 'trade'],
+                    },
+                })
+            }
+        }
+
+        this.ws.onData = data => {
+            console.log('data', data)
+        }
+
     }
 }
