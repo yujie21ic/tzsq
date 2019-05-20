a7f8a005610c9048e5c67142b9f016b5688fcd64
fc01 <910677221@qq.com>
2019-05-20 21:54:49 +0800

    `

--- src/RealDataServer/TradeAndOrderBook/DeribitTradeAndOrderBook.ts
+++ src/RealDataServer/TradeAndOrderBook/DeribitTradeAndOrderBook.ts
@@ -2,6 +2,43 @@ import { TradeAndOrderBook } from './TradeAndOrderBook'
 import { BaseType } from '../../lib/BaseType'
 import { WebSocketClient } from '../../lib/F/WebSocketClient'
 import { config } from '../../config'
+import { createHash } from 'crypto'
+
+const KEY = 'aa'
+const SEC = 'bb'
+
+const serialize = (m: any) =>
+    Object.keys(m)
+        .sort()
+        .map(k => (Array.isArray(m[k]) ? `${k}=${m[k].join('')}` : `${k}=${m[k]}`))
+        .join('&')
+
+
+const sig = (action: string, obj: any) => {
+    const time = Date.now()
+    const hash = createHash('sha256')
+    hash.update(
+        serialize({
+            ...obj,
+            _: time,
+            _ackey: KEY,
+            _acsec: SEC,
+            _action: action,
+        })
+    )
+    const sig = `${KEY}.${time}.${hash.digest('base64')}`
+    return sig
+}
+
+const privateAction = (action: string, obj: any) => (
+    {
+        'id': 1234,
+        'action': action,
+        'arguments': obj,
+        'sig': sig(action, obj),
+    }
+)
+
 
 export class DeribitTradeAndOrderBook extends TradeAndOrderBook<BaseType.BitmexSymbol> {
 
@@ -20,15 +57,12 @@ export class DeribitTradeAndOrderBook extends TradeAndOrderBook<BaseType.BitmexS
             this.statusObservable.next({ isConnected: this.ws.isConnected })
 
             if (this.ws.isConnected) {
-                this.ws.sendJSON({
-                    'id': 1234,
-                    'action': '/api/v1/private/subscribe',
-                    'arguments': {
+                this.ws.sendJSON(
+                    privateAction('/api/v1/private/subscribe', {
                         'instrument': ['BTC-PERPETUAL'],
                         'event': ['order_book', 'trade'],
-                    },
-                    'sig': '??????????????????????????????',
-                })
+                    })
+                )
             }
         }
 
