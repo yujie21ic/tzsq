c408c78f96b07ed3002cb0b82a677371caae123f
samuel <472144905@qq.com>
2019-06-05 17:33:51 +0800

    、

--- src/HopexKLine.ts
+++
@@ -1,6 +0,0 @@
-import { theme } from './Chart/theme'
-import { HopexRealKLine } from './RealDataServer/RealKLine/HopexRealKLine'
-import { RealKLineChart } from './RealKLineChart'
-
-theme.右边空白 = 0
-RealKLineChart(document.querySelector('#root') as HTMLElement, new HopexRealKLine())
