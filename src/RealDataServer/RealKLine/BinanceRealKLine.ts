7bfcabf04c3eb828c3184958668a47ef0cc4db43
fc01 <910677221@qq.com>
2019-06-05 14:31:08 +0800

    ``

--- src/RealDataServer/RealKLine/BinanceRealKLine.ts
+++ src/RealDataServer/RealKLine/BinanceRealKLine.ts
@@ -1,5 +1,9 @@
 import { RealKLineBase } from './RealKLineBase'
 
+// import { JSONRequest } from '../../F/JSONRequest'    //http
+// import { WebSocketClient } from '../../F/WebSocketClient'  //ws
+
+
 export class BinanceRealKLine extends RealKLineBase {
 
     constructor() {
